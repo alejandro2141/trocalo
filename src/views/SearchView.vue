ddb145cb7a33c0e64931897cd86ad21caebf47c9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-28 20:25:11 -0300

    Create SearchView.vue

---
+++ src/views/SearchView.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+import HomeView from './views/SearchFilter.vue'
+import Filter from '../components/FilterForSearchView.vue'
+import SearchResult from '../components/SearchResult.vue'
+
+</script>
+
+<template>
+ 
+  <Filter/>
+<br>
+  <SearchResult/>
+
+
+</template>
+
+<style>
+
+</style>
