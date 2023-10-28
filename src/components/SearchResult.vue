e90bca8c5cee2b7c34606604170c48c78fe6fc10
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-28 20:25:21 -0300

    Create SearchResult.vue

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import InventoryObject from './InventoryObject.vue'
+</script>
+
+<template>
+
+  <div class="w-100">
+
+    <div class="d-flex justify-content-between">
+        Objetos
+    </div>
+    <div class="d-flex flex-wrap"> 
+    
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+
+        
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
