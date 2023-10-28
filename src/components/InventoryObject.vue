aea2fbd35ff6b49d530586458549878e5a4709f4
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-28 20:25:23 -0300

    Create InventoryObject.vue

---
+++ src/components/InventoryObject.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div class="">
+
+    <div class="bg-secondary" style="width:100px">
+        <div>
+        Play station 3
+        </div>
+        <div>
+        IMAGE
+        </div>
+        No tiene reallados, con solo un control, no tienen juegos y esta desbloqueada
+        <br>
+        xbox con juegos 
+        Bicicleta aro 23
+        Otros
+
+    </div> 
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
