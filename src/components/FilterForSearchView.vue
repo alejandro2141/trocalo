f8c0bb6fa732a0187b86d95bc4e3213aa8414ee9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-28 20:25:25 -0300

    Create FilterForSearchView.vue

---
+++ src/components/FilterForSearchView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <div class="d-flex justify-content-between">
+        <text>Juegos</text>
+        <text>|</text>
+        <text>Consolas</text>
+        <text>|</text>
+        <text>Electro</text>
+        <text>|</text>
+        <text>Hogar</text>
+        <text>|</text>
+        <text>Accesorios</text>
+        <text>|</text>
+        <text>+Mas</text> 
+    </div>
+    <br>
+    <input type="text" id="lname" name="lname"   placeholder="Buscar" >
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
