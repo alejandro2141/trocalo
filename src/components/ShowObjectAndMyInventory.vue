eb8ef95dddfe86e51e05ec3f9d35618207b34305
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-30 23:36:20 -0300

    Create ShowObjectAndMyInventory.vue

---
+++ src/components/ShowObjectAndMyInventory.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <div class="w-100 m-2 border border-1 bg-secondary"  >
+    SHow Object and My Inventory
+  </div>
+  <InventoryObjectDetailed/>
+  <InventoryList/>
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+import InventoryObjectDetailed from './InventoryObjectDetailed.vue'
+import InventoryList from './InventoryList.vue'
+
+
+export default {
+
+  components: {
+    InventoryObjectDetailed,InventoryList
+  },
+  
+  data : function() {
+      return {
+        showModalDetails: false
+      }
+  },
+  props: ['session_data'],
+  emits: ['closeThisModal'],
+
+created() {
+  console.log("APP CREATED")
+    },
+
+methods: {
+    
+    closeModalObjectDetails()
+    {
+      this.showModalDetails= false
+    }
+
+    },
+
+watch : {
+      }
+}
+</script>
+
