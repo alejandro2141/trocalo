0d27488bbd3807595d5c960f82d2e4b6e81e2168
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-30 23:36:27 -0300

    Update InventoryObject.vue

--- src/components/InventoryObject.vue
+++ src/components/InventoryObject.vue
@@ -1,20 +1,27 @@
+<script setup>
+</script>
+
 <template>
 
   <div class="">
 
-    <div class="bg-secondary" style="width:100px">
+    <div class="border border-1" style="width:100px">
         <div>
-        Play station 3
+            Play station 3
         </div>
-        <div>
-        IMAGE
+        <div class="bg-secondary" style="width:100xp; height:100px">
+            IMAGE
         </div>
-        No tiene reallados, con solo un control, no tienen juegos y esta desbloqueada
+        <div v-if="showProductDetails">
+            No tiene reallados, con solo un control, no tienen juegos y esta desbloqueada
+        </div>
         <br>
-        xbox con juegos 
-        Bicicleta aro 23
-        Otros
-
+        <div v-if="showProductPreference">
+            xbox con juegos 
+            Bicicleta aro 23
+            Otros
+        </div>
+        
     </div> 
 
   </div>
@@ -24,3 +31,34 @@
 <style scoped>
 
 </style>
+
+
+<script>
+//const showForm = ref(false)
+
+
+export default {
+  
+    data : function() {
+        return {
+        lala : true ,
+        }
+    },
+
+    props: ['showProductDetails','showProductPreference' ],
+    emits: [],
+
+	created() {
+		console.log("created lala inventory Object")
+	
+			},
+
+
+
+  methods: {
+      },
+
+  watch : {
+        }
+}
+</script>
