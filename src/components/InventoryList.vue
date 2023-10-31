dbfc5599e0bbeba9aaff1079433769b42b50f079
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-10-30 23:36:30 -0300

    Create InventoryList.vue

---
+++ src/components/InventoryList.vue
@@ -0,0 +1,73 @@
+<template>
+
+  <div class="w-100 m-2 border border-1 bg-secondary"  >
+
+   
+
+    <div @click="$emit('closeThisModal')" class="">
+         X
+    </div>
+    <div class="d-flex justify-content-between">
+        Selecciona Objetos de tu Inventario
+    </div>
+    <div class="d-flex flex-wrap"> 
+    
+      <InventoryObject @click="showModalDetails=true" />
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+      <InventoryObject class="m-1"/>
+        
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+import InventoryObject from './InventoryObject.vue'
+
+export default {
+
+  components: {
+    InventoryObject
+  },
+  
+  data : function() {
+      return {
+        showModalDetails: false
+      }
+  },
+  props: ['session_data'],
+  emits: ['closeThisModal'],
+
+created() {
+  console.log("APP CREATED")
+    },
+
+methods: {
+    
+    closeModalObjectDetails()
+    {
+      this.showModalDetails= false
+    }
+
+    },
+
+watch : {
+      }
+}
+</script>
+
