d167287f6cb46af5afe4426caf0f848173c509ee
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 00:44:13 -0300

    Update ShowObjectAndMyInventory.vue

--- src/components/ShowObjectAndMyInventory.vue
+++ src/components/ShowObjectAndMyInventory.vue
@@ -1,11 +1,82 @@
 <template>
+<div style="width:400px">
 
-  <div class="w-100 m-2 border border-1 bg-secondary"  >
-    SHow Object and My Inventory
+<div v-if="showSenderConfirmation" class="position-absolute top-50 start-50 translate-middle bg-dark">
+  <text @click="showSenderConfirmation=false">x</text>
+  <ShowSenderTransactionConfirmation />
+</div>
+
+  <div class="w-100 m-2 "  >
+    Propuesta Intercambio   <a href="/"> X </a>
   </div>
-  <InventoryObjectDetailed/>
-  <InventoryList/>
+  Objeto de tu interes
 
+  <!-- Desire Object + Plus -->
+   <div class="d-flex align-content-stretch flex-wrap">
+        <InventoryObject      @click="showPartnerObjectDetailed=true"/> 
+        <InventoryObjectEmpty @click="showPartnerInventory=true" />
+    </div>
+    
+  <!-- Show Inventory Partner -->
+    <div v-if="showPartnerInventory">
+      <text @click="showPartnerInventory=false">X</text> 
+      <InventoryList />
+    </div>
+
+  <!-- Show Object Details  -->  
+    <div v-if="showPartnerObjectDetailed">
+      <text @click="showPartnerObjectDetailed=false">X</text> 
+      <InventoryObjectDetailed />
+    </div>
+
+  Selecciona Objetos de tu inventario para intercambiar
+
+  <!-- Desire Object + Plus -->
+  <div class="d-flex align-content-stretch flex-wrap">
+        <InventoryObject      @click="showMyObjectDetailed=true"/> 
+        <InventoryObjectEmpty @click="showMyInventory=true" />
+    </div>
+  <!-- Show Object Details  -->  
+    <div v-if="showMyObjectDetailed">
+      <text @click="showMyObjectDetailed=false">X</text> 
+      <InventoryObjectDetailed />
+    </div>  
+  <!-- Show Inventory Partner -->
+    <div v-if="showMyInventory">
+      <text @click="showMyInventory=false">X</text> 
+      <InventoryList />
+    </div>
+
+    <br>
+
+    <div class="form-check">  
+      <label class="form-check-label" for="flexCheckChecked">
+       Abierto a intercambiar por otros productos. 
+      </label>
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+    </div>
+
+    <div>
+      Esta propuesta será valida por: 
+      <div class="w-25">
+          <select class="form-control" id="sel1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+      </div>
+    </div>
+    <br>
+
+    <div>
+      $39.000 es el costo de Retiro y despacho de estos productos que usted debera pagar. 
+    </div>
+
+    <button @click="showSenderConfirmation=true" type="button" class="btn btn-success">Enviar</button>
+    <button type="button" class="btn btn-danger">Cancelar</button>
+
+</div>
 </template>
 
 <style scoped>
@@ -15,19 +86,29 @@
 
 <script>
 import InventoryObjectDetailed from './InventoryObjectDetailed.vue'
+import InventoryObject from './InventoryObject.vue'
 import InventoryList from './InventoryList.vue'
+import InventoryObjectEmpty from './InventoryObjectEmpty.vue'
+import ShowSenderTransactionConfirmation from './ShowSenderTransactionConfirmation.vue'
 
 
 export default {
 
   components: {
-    InventoryObjectDetailed,InventoryList
+    InventoryObjectDetailed,InventoryList,InventoryObjectEmpty,InventoryObject,ShowSenderTransactionConfirmation
   },
   
   data : function() {
       return {
-        showModalDetails: false
-      }
+        //showModalDetails: false,
+        //showYourInventory : false ,
+        showPartnerInventory : false ,
+        showPartnerObjectDetailed: false ,
+
+        showMyObjectDetailed: false,
+        showMyInventory: false, 
+        showSenderConfirmation : false ,
+        }
   },
   props: ['session_data'],
   emits: ['closeThisModal'],
@@ -41,7 +122,12 @@ methods: {
     closeModalObjectDetails()
     {
       this.showModalDetails= false
-    }
+    },
+
+    goToMainSearch()
+    {
+      console.log("goTOMainSearch")
+    },
 
     },
 
