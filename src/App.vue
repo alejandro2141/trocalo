b6bf4ca76a7f44596acce70505ab8cd17b1b931b
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:19:06 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 
 <template>
-  <div class="m-1 "  style="margin:0px; padding:0px;  width: 450px;">
+  <div class="m-4 p-0"  style="width: 350px;">
 
-    <div class=" d-flex justify-content-around">
+    <div class=" d-flex justify-content-between">
         <!-- <a href="/">Buscar</a>-->
         <RouterLink  class="" to="/searchView">Buscar</RouterLink>    
         <RouterLink v-if="session_data.user!=null"  class="" to="/login">{{session_data.user}}</RouterLink>
         <RouterLink v-else  class="" to="/login">Tu Cuenta</RouterLink>
     </div> 
 
+    <div v-if="session_data!=null && session_data.user!=null" class="d-flex justify-content-between" >
+      <RouterLink  class="" to="/MyInventory">Inventario</RouterLink>    
+      <RouterLink  class="" to="/OfReceived">Recibidas</RouterLink>    
+      <RouterLink  class="" to="/OfSent">Enviadas</RouterLink>    
+    </div>
+
+
+
+    <RouterView  v-on:sessionCreated="sessionCreated"   :session_data='session_data' />
   </div>
-  <br>
-  
-  <RouterView  v-on:sessionCreated="sessionCreated"   :session_data='session_data' />
+
+
+ 
+
+
+ 
  
 </template>
 
@@ -26,8 +38,12 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 
+
 export default {
   
+  components: {
+    },
+
     data : function() {
         return {
           session_data : {user:"JUan Alejandro Morales", pass:"eeee"}
