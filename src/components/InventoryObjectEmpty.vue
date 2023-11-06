4196c96fdfd59260ed503f02c792c3fc31427ae2
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 00:44:21 -0300

    Create InventoryObjectEmpty.vue

---
+++ src/components/InventoryObjectEmpty.vue
@@ -0,0 +1,65 @@
+<script setup>
+</script>
+
+<template>
+
+  <div class="m-2">
+
+    <div class="border border-1 rounded" style="width:100px">
+        <div>
+         
+        </div>
+        
+        <div class="bg-dark" style="">
+            <i class="display-1 bi bi-plus-lg"></i>
+        </div>
+
+
+        <div v-if="showProductDetails">
+           aaa
+        </div>
+        <br>
+        <div v-if="showProductPreference">
+           ffffff
+        </div>
+        
+    </div> 
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+//const showForm = ref(false)
+
+
+export default {
+  
+    data : function() {
+        return {
+        lala : true ,
+        }
+    },
+
+    props: ['showProductDetails','showProductPreference' ],
+    emits: [],
+
+	created() {
+		console.log("created lala inventory Object")
+	
+			},
+
+
+
+  methods: {
+      },
+
+  watch : {
+        }
+}
+</script>
