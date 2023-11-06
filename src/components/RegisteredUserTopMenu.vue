03d0f51c525f4b962acaad7921c0579b542cdf82
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:50 -0300

    Create RegisteredUserTopMenu.vue

---
+++ src/components/RegisteredUserTopMenu.vue
@@ -0,0 +1,42 @@
+<script setup>
+</script>
+
+<template>
+
+  <div class="d-flex justify-content-around" >
+
+    <div>Inventario</div>
+    <div>Recibidas</div>
+    <div>Enviadas</div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+export default {
+  
+    data : function() {
+        return {
+        }
+    },
+
+    props: [],
+    emits: [],
+
+	created() {
+			},
+
+  methods: {
+      },
+
+  watch : {
+        }
+}
+</script>
