d4320d3d38f09d4e79723c13c444a72b6f8876ce
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:21 -0300

    Create OfReceived.vue

---
+++ src/views/OfReceived.vue
@@ -0,0 +1,43 @@
+
+<template>
+ 
+ <div>
+    Received
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+  
+
+export default {
+
+
+  components: {
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
