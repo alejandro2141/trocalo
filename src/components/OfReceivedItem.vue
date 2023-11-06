e1c2ee348a3e02c542af020fa03168c3ecf67e2a
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:54 -0300

    Create OfReceivedItem.vue

---
+++ src/components/OfReceivedItem.vue
@@ -0,0 +1,55 @@
+
+<template>
+ 
+ <div>
+
+    <div class="border border-1 rounded">
+        Cakito_123  se interesa x:<br>
+        Objeto 
+    </div>
+
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import InventoryObject from '../components/InventoryObject.vue'
+import InventoryObjectDetailed from '../components/InventoryObjectDetailed.vue'
+import InventoryList from '../components/InventoryList.vue' 
+
+
+
+export default {
+
+
+  components: {
+    InventoryObject,InventoryObjectDetailed,InventoryList
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
