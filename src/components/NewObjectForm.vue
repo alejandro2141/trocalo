6cd041e601b1fc5bcbcb79c2feea6038eabdcd20
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:57 -0300

    Create NewObjectForm.vue

---
+++ src/components/NewObjectForm.vue
@@ -0,0 +1,54 @@
+
+<template>
+ 
+ <div>
+
+    <div>
+        New Object Form
+    </div>
+
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import InventoryObject from '../components/InventoryObject.vue'
+import InventoryObjectDetailed from '../components/InventoryObjectDetailed.vue'
+import InventoryList from '../components/InventoryList.vue' 
+
+
+
+export default {
+
+
+  components: {
+    InventoryObject,InventoryObjectDetailed,InventoryList
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
