1afb289fa7a804fcd471ba091e8de871141bf9db
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:16 -0300

    Create OfSent.vue

---
+++ src/views/OfSent.vue
@@ -0,0 +1,42 @@
+
+<template>
+ 
+ <div>
+    Sent
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+
+export default {
+
+  components: {
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
