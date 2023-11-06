8bcca316eb9007589a9b14a94503bee2669a2629
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-06 19:18:32 -0300

    Create MyInventory.vue

---
+++ src/views/MyInventory.vue
@@ -0,0 +1,62 @@
+
+<template>
+ 
+ <div>
+
+    <div v-if="!showNewObjectForm">
+        <i  @click="showNewObjectForm=true" class="display-1 bi bi-plus-lg"></i>
+        <br>
+        <InventoryList />
+    </div>
+    <div v-else>
+      <i  @click="showNewObjectForm=false" class="display-1 bi bi-x-lg"></i>
+          <NewObjectForm />
+    </div>
+
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import InventoryObject from '../components/InventoryObject.vue'
+import InventoryObjectDetailed from '../components/InventoryObjectDetailed.vue'
+import InventoryList from '../components/InventoryList.vue' 
+import NewObjectForm from '../components/NewObjectForm.vue'
+
+
+
+export default {
+
+
+  components: {
+    InventoryObject,InventoryObjectDetailed,InventoryList,NewObjectForm
+  },
+  
+  data : function() {
+      return {
+        showNewObjectForm : false, 
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
