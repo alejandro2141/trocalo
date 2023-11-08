6931bee85eb673470bb0833617bcb980a62be509
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-08 19:33:08 -0300

    Create OfReceivedItemList.vue

---
+++ src/components/OfReceivedItemList.vue
@@ -0,0 +1,53 @@
+
+<template>
+ 
+ <div>
+Of received Item List
+    <OfReceivedItem />
+    <OfReceivedItem />
+    <OfReceivedItem />
+    <OfReceivedItem />
+    <OfReceivedItem />
+    <OfReceivedItem />
+
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import OfReceivedItem from '../components/OfReceivedItem.vue'
+
+export default {
+
+
+  components: {
+    OfReceivedItem
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
