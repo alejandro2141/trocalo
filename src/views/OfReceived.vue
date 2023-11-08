0f1459480ff45a6738f288f7c47f4028f51898b2
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-08 19:33:03 -0300

    Update OfReceived.vue

--- src/views/OfReceived.vue
+++ src/views/OfReceived.vue
@@ -1,9 +1,23 @@
 
 <template>
  
- <div>
-    Received
- </div>
+<div>
+
+    <div v-if="showItemList" >
+        <OfReceivedItem @click="showItemList=false" />
+        <OfReceivedItem />
+        <OfReceivedItem />
+        <OfReceivedItem />
+        <OfReceivedItem />
+        <OfReceivedItem />
+        
+    </div>
+    <div v-else >
+        <i @click="showItemList=true" class="display-1 bi bi-x-lg"></i>
+        <ShowObjectAndMyInventory />
+    </div>
+ 
+</div>
 
 </template>
 
@@ -14,15 +28,21 @@
 
 <script>
   
+import OfReceivedItemList from '../components/OfReceivedItemList.vue'
+import OfReceivedItem from '../components/OfReceivedItem.vue'
+import InventoryList  from '../components/InventoryList.vue'
+import ShowObjectAndMyInventory from '../components/ShowObjectAndMyInventory.vue'
 
 export default {
 
 
   components: {
+    OfReceivedItemList,OfReceivedItem,InventoryList,ShowObjectAndMyInventory
   },
   
   data : function() {
       return {
+        showItemList:true ,
 
       }
   },
