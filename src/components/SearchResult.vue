7b4828155a8a0b570f6b967e283295871daf0924
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-14 20:58:47 -0300

    Update SearchResult.vue

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <div class="w-100">
+  <div class="w-100 ">
 
 
-    <div v-if=" !showModalDetails && !showObjectAndMyInventoryModal ">
+    <div class="mt-2" v-if=" !showModalDetails && !showExchangeProposal ">
         <FilterForSearchView  />
     </div>
 
@@ -11,10 +11,8 @@
     <InventoryList  v-on:closeThisModal="closeInventoryList" v-if="showModalInventoryList" />
     -->
     <!-- SHOW PUBLIC OBJECTS ARE AVAILABLE FOR SEARCH-->
-    <div v-if="showObjectAndMyInventoryModal==false && showModalDetails==false  ">
-        <div class="d-flex justify-content-between">
-            Objetos Publicos
-        </div>
+    <div v-if="showExchangeProposal==false && showModalDetails==false  ">
+        
         <div class="d-flex flex-wrap"> 
         
           <InventoryObject @click="showModalDetails=true" :showProductDetails="false" :showProductPreference="false"   class="m-1"/>
@@ -34,34 +32,67 @@
 
 
     <!-- 1 Show Object Details-->
-    <div v-if="showModalDetails">
-      <text @click="showModalDetails=false ">X</text>
-      <InventoryObjectDetailed  v-on:showMyInventory='showMyInventory=true'  v-on:closeModalObjectDetails="closeModalObjectDetails" :session_data="session_data" />
+    <div v-if="showModalDetails" class="position-absolute top-0 start-10 bg-dark" >
+          <div class="" >
+                  <div class="d-flex justify-content-end"> 
+                  <i @click="showModalDetails=false" class="bi bi-x-lg display-1" ></i>
+                  </div>
+                  <InventoryObjectDetailed  v-on:showMyInventory='showMyInventory=true'  v-on:closeModalObjectDetails="closeModalObjectDetails" :session_data="session_data" />
+          </div>
+          
+          <div class="m-4 pb-4" v-if="showModalDetails">
+                  <button v-if="session_data!=null && session_data.user!=null && !showMyInventory" @click="showExchangeProposal=true ; showModalDetails=false  " type="button" class="btn btn-primary">Hacer oferta por este producto</button>
+                  <button v-else  type="button" class="btn btn-secondary">Debe estar registrado para ofertar</button>
+          </div>        
     </div>
-      <!-- Buttons to go 2-->
-      <div>
-        <button v-if="session_data!=null && session_data.user!=null && !showMyInventory" @click="showMyInventory=true" type="button" class="btn btn-primary">Hacer oferta por este producto</button>
-        <button v-else  type="button" class="btn btn-secondary">Debe estar registrado para ofertar</button>
+
+     <!-- 1 Show Object Details-->
+     <div v-if="showExchangeProposal" class="position-absolute top-0 start-10 bg-dark" >
+          <div class="d-flex justify-content-end"> 
+            <i @click="showExchangeProposal=false ;  showModalDetails=true" class="bi bi-x-lg display-1" ></i>
+          </div>
+         
+         
+          <div>
+            <ExchangeProposal/>
+          </div>        
+    </div>
+
+
+
+
+    <!-- 1 Show Object Details-
+    <div v-if="showModalDetails" class="" >
+
+      <div class="d-flex justify-content-end"> <i @click="showModalDetails=false" class="bi bi-x-lg display-1" ></i></div>
+          <InventoryObjectDetailed  v-on:showMyInventory='showMyInventory=true'  v-on:closeModalObjectDetails="closeModalObjectDetails" :session_data="session_data" />
       </div>
 
-    <!-- 2 Show FORM to Share-->
+      <div class="m-4 pb-4" v-if="showModalDetails">
+          <button v-if="session_data!=null && session_data.user!=null && !showMyInventory" @click="showMyInventory=true" type="button" class="btn btn-primary">Hacer oferta por este producto</button>
+          <button v-else  type="button" class="btn btn-secondary">Debe estar registrado para ofertar</button>
+      </div>        
+
+    </div>
+  -->
+    <!-- 2 Show FORM to Share
     <div v-if="showMyInventory">
         <InventoryList />
-    </div>
-      <!-- Buttons to go 3-->
+    </div>-->
+      <!-- Buttons to go 3
       <div v-if="showMyInventory">
         <button @click="showObjectAndMyInventoryModal=true ; showMyInventory=false ; showModalDetails=false "  type="button" class="btn btn-secondary">ENVIAR </button>
       </div>
-        
-    <!-- 3 Show FORM to Share-->
+        -->
+    <!-- 3 Show FORM to Share
     <div v-if="showObjectAndMyInventoryModal">
         <ShowObjectAndMyInventory  /> 
     </div>
+-->
 
 
-
-
-  </div>
+</div>
+  
 
 </template>
 
@@ -74,14 +105,14 @@
 
 import InventoryObject from './InventoryObject.vue'
 import InventoryObjectDetailed from './InventoryObjectDetailed.vue'
-import ShowObjectAndMyInventory from './ShowObjectAndMyInventory.vue'
 import FilterForSearchView from '../components/FilterForSearchView.vue'
 import InventoryList from '../components/InventoryList.vue' 
+import ExchangeProposal from '../components/ExchangeProposal.vue'
 
 export default {
 
   components: {
-    InventoryObject,InventoryObjectDetailed,ShowObjectAndMyInventory,FilterForSearchView,InventoryList
+    InventoryObject,InventoryObjectDetailed,FilterForSearchView,InventoryList,ExchangeProposal
   },
   
   data : function() {
@@ -89,7 +120,7 @@ export default {
         showModalDetails: false,
         showMyInventory:false ,
       
-        showObjectAndMyInventoryModal : false  
+        showExchangeProposal : false  
       }
   },
   props: ['session_data'],
