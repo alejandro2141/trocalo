8966ef341e14ad7f4e7fa588d08af25893821142
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-14 20:58:56 -0300

    Update InventoryObject.vue

--- src/components/InventoryObject.vue
+++ src/components/InventoryObject.vue
@@ -3,25 +3,49 @@
 
 <template>
 
-  <div class="">
+  <div class=""   >
 
-    <div class="border border-1" style="width:100px">
-        <div>
-            Play station 3
-        </div>
-        <div class="bg-secondary" style="width:100xp; height:100px">
-            IMAGE
-        </div>
-        <div v-if="showProductDetails">
-            No tiene reallados, con solo un control, no tienen juegos y esta desbloqueada
-        </div>
-        <br>
-        <div v-if="showProductPreference">
-            xbox con juegos 
-            Bicicleta aro 23
-            Otros
+
+    <div v-if="showConfirmMode" class="d-flex justify-content-start" >
+                <div class="bg-white"  style="width:100px; height:100px">
+                IMAGE
+                </div>
+                <div  class="d-flex align-items-center m-2">
+                    Play station 3
+                </div>
+    </div>
+    <div v-else> 
+
+    <div  @click="selectObject=!selectObject"  class="border border-2 rounded" :class="[selectObject ? 'border-white' : 'border-dark' , '' ]"  style="width:100px ; border-style: dotted"   >
+    
+
+        
+        
+        
+                <div class="bg-white"  style="width:100px; height:100px">
+                IMAGE
+                </div>
+                <div>
+                    Play station 3
+                </div>
+                
+                <div v-if="showProductDetails" class="text-secondary">
+                    No tiene reallados, con solo contro ...
+                </div>
+                <div v-if="showDeleteOption" class="d-flex flex-row-reverse text-danger"> <i class="bi bi-x-lg display-5"></i> </div>
+
+                <br>
+                <div v-if="showProductPreference">
+                    xbox con juegos 
+                    Bicicleta aro 23
+                    Otros
+                </div>
+
         </div>
         
+        
+        
+    
     </div> 
 
   </div>
@@ -42,10 +66,11 @@ export default {
     data : function() {
         return {
         lala : true ,
+        selectObject : false ,
         }
     },
 
-    props: ['showProductDetails','showProductPreference' ],
+    props: ['showProductDetails','showProductPreference','isActive','showDeleteOption', 'showConfirmMode' ],
     emits: [],
 
 	created() {
@@ -59,6 +84,12 @@ export default {
       },
 
   watch : {
+
+            isActive(newval)
+            {
+                console.log("selected "+newval);
+            },
+
         }
 }
 </script>
