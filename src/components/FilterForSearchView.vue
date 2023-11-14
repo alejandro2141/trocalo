cfd5f9fb46908cf0c0f4e71da8638791530611c5
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-14 20:59:01 -0300

    Update FilterForSearchView.vue

--- src/components/FilterForSearchView.vue
+++ src/components/FilterForSearchView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="">
-    <div class="d-flex justify-content-between">
+  <div class="" style="font-size:20px">
+    <div class="">
         <text>Juegos</text>
-        <text>|</text>
+        <text>| </text> 
         <text>Consolas</text>
-        <text>|</text>
+        <text>| </text>
         <text>Electro</text>
-        <text>|</text>
+        <text>| </text>
         <text>Hogar</text>
-        <text>|</text>
+        <text>| </text>
         <text>Accesorios</text>
-        <text>|</text>
-        <text>+Mas</text> 
+        <text>| </text>
+        <text>+Mas </text> 
+         &nbsp;&nbsp; &nbsp;  <input type="text" placeholder="Buscar" id="searchText" name="searchText" class="form-control-sm mt-1" required minlength="4" maxlength="20" size="7" /><i class="p-1 bg-white text-dark bi bi-search-heart"></i>
     </div>
-    <br>
-    <input type="text" id="lname" name="lname"   placeholder="Buscar" >
-
+    <br> 
+   
   </div>
 </template>
 
