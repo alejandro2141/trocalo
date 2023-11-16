3e78843ec9f14239d77a0f92cf4e58e7ec99c51a
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-16 19:44:56 -0300

    Update NewObjectForm.vue

--- src/components/NewObjectForm.vue
+++ src/components/NewObjectForm.vue
@@ -3,16 +3,106 @@
  
  <div>
 
+
+  <div v-if="step1">
+
     <div>
-        New Object Form
+      <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class="form-control mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
     </div>
 
+    <div class="d-flex flex-wrap w-100">
+
+      <div style="width:150px; height:150px" class="bg-white m-1 ">
+      </div>
+
+      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      </div>
+
+      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      </div>
+
+      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      </div>
+
+      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      </div>
+
+    </div>
+
+    <div>
+      <textarea class="form-control mt-1 bg-dark text-white border-white" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    
+    <br>
+    <div>
+        <p style="font-size:20px">
+          Cambiaria Por: 
+        </p>
+        <div>
+          <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class="form-control mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
+        </div>
+        <div>
+          <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class="form-control mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
+        </div>
+        <div>
+          <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class="form-control mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
+        </div>
+    </div>
+    
+    <br>
+    <div>
+        <p style="font-size:20px">
+         Cambiaria por otros Objetos? <br> <button type="button" class="btn btn-dark border-white">NO</button>  <button type="button" class="btn btn-success">SI</button>
+        </p>
+    </div>
+
+    <br>
+    <div class="d-flex justify-content-center display-1 text-success w-100 bg-dark p-3 " >
+        <i @click="step1=false;showRequestConfirmation=true" class="bi bi-cloud-upload"></i>
+    </div>
+    
+
+  </div>
+
+
+  <div v-if="showRequestConfirmation"  style="width:350px;height:500px">
+     <div class="position-absolute top-0  text-center start-0 bg-dark w-100 h-100" >
+        <div>
+            <!-- SPACE FILLER -->
+            <p style="height:300px">
+            </p>
+            Articulo Guardado con Exito en tu Inventario
+        </div>
+      <br>
+        <div class="d-flex justify-content-center">
+           <div class="fixed-bottom display-1 text-success w-100 bg-dark p-3 ">  
+              <div class="d-flex justify-content-center">
+
+                <a href="/">
+                <i class="text-success bi bi-house" style="font-size:40px "></i> 
+                </a>
+              
+              </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+     <!-- SPACE FILLER -->
+     <p style="height:300px">
+    </p>
 
  </div>
 
 </template>
 
 <style>
+input::placeholder {
+  color: white;
+  font-size: 1.2em;
+  font-style: italic;
+}
 
 </style>
 
@@ -34,6 +124,8 @@ export default {
   
   data : function() {
       return {
+        step1 : true ,
+        showRequestConfirmation: false,
 
       }
   },
