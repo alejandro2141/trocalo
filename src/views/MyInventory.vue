23654eaa721e8480178d1de57b18d5474ca7e1dc
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-17 18:34:51 -0300

    Update MyInventory.vue

--- src/views/MyInventory.vue
+++ src/views/MyInventory.vue
@@ -1,18 +1,17 @@
-
 <template>
  
  <div>
 
     <div v-if="!showNewObjectForm">
         <div class="d-flex justify-content-center">
-            <i  @click="showNewObjectForm=true" style="font-size:90px;" class="bi bi-plus-lg"></i>
+            <i  @click="showNewObjectForm=true" style="font-size:50px;" class="bi bi-plus-lg"></i>
         </div>
         <br>
         <InventoryList />
     </div>
     <div v-else  class="position-absolute top-0 start-10 bg-dark" >
-        <div style="transition: width 2s;" class="d-flex justify-content-center">
-            <i  @click="showNewObjectForm=false" style="font-size:70px;" class="display-1 bi bi-x-lg"></i>
+        <div style="transition: width 2s;" class="d-flex justify-content-end">
+            <i  @click="showNewObjectForm=false" style="font-size:50px;" class="display-1 bi bi-x-lg"></i>
         </div>
         <NewObjectForm />
     </div>
