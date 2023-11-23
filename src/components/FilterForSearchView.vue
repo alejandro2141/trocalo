9120179e59aeabd96ef643a5d8d404b838edb143
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-23 18:51:57 -0300

    Update FilterForSearchView.vue

--- src/components/FilterForSearchView.vue
+++ src/components/FilterForSearchView.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="" style="font-size:20px">
-    <div class="">
-        <text>Juegos</text>
+  <div class="" style="font-size:30px">
+    <div class="text-primary ">
+        <text><i class="bi bi-controller"></i></text>
         <text>| </text> 
-        <text>Consolas</text>
+        <text><i class="bi bi-bicycle"></i></text>
         <text>| </text>
-        <text>Electro</text>
-        <text>| </text>
-        <text>Hogar</text>
-        <text>| </text>
-        <text>Accesorios</text>
-        <text>| </text>
-        <text>+Mas </text> 
-         &nbsp;&nbsp; &nbsp;  <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class="form-control-sm mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
         
+        <text><i class="bi bi-tools"></i></text>
+        <text>| </text>
+        <text><i class="bi bi-music-player"></i></text>
+        <text>| </text>
+    <!--
+        <text><i class="bi bi-house"></i></text>
+        <text>| </text>
+        <text><i class="bi bi-book"></i></text>
+        <text>| </text>
+        <text><i class="bi bi-car-front"></i></text>
+        <text>| </text>
+    -->
+        <text>... </text> 
+
+        <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-primary" required minlength="4" maxlength="20" size="7" />
+         
+
     </div>
-    <br> 
-   
+    
   </div>
 </template>
 
