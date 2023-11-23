b6c73d32a6971f726338aa35b77589d06ce18c7c
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-23 18:52:00 -0300

    Update ExchangeProposalReceived.vue

--- src/components/ExchangeProposalReceived.vue
+++ src/components/ExchangeProposalReceived.vue
@@ -9,28 +9,29 @@
   <div v-if="showStep1" >
         <p style="font-size:20px" >Propuesta de Intercambio Recibida</p>
        <!-- TITLE -->
-          <div style="font-size:20px" class="d-flex justify-content-between">
-              <div>Propuesta válida por: 25 dias </div>
+          <div style="font-size:20px" class="mb-4 d-flex justify-content-between">
+              <div>Propuesta válida por:  25 dias </div>
           </div>       
+          
           <div  style="font-size:16px "  class="d-flex justify-content-center">
               Kakito_123 esta interesado en los siguientes objetos:  
           </div>
-         
-          <!-- LIST PARTNER OBJECT-->
-          <div  class="">
-              <InventoryObject class="m-1" :showConfirmMode="true"  :showProductDetails="false" /> 
-              <InventoryObject class="m-1" :showConfirmMode="true"  :showProductDetails="false" /> 
+          <!-- LIST MY OFFER OBJECT  -->
+          <div v-for="obj in yourOfferObjects" :key="index" class="mb-4" > 
+              <InventoryObject @click="showModalDetails=true; objectDetails=obj" :object="obj"  :showConfirmMode="true"  :showProductDetails="false" class="mb-1" /> 
           </div>
-          <!-- END LIST PARTNER OBJECT-->
-          
+          <!-- END LIST MY OFFER OBJECT  --> 
+         
           <div  style="font-size:16px "  class="m-2">
             Y propone cambiarlo por los siguientes objetos: 
           </div>
-           <!-- LIST MY OBJECT  -->
-          <div  class="d-flex align-content-stretch flex-wrap">
-             <InventoryObject class="m-1" :showConfirmMode="true"  :showProductDetails="false" /> 
-             <InventoryObject class="m-1" :showConfirmMode="true"  :showProductDetails="false" /> 
+           <!-- LIST PARTNER OFFER OBJECT  -->
+          <div v-for="obj in partnerOfferObjects" :key="index" > 
+              <InventoryObject @click="showModalDetails=true; objectDetails=obj" :object="obj"  :showConfirmMode="true"  :showProductDetails="false"  class="mb-1"  /> 
           </div>
+          <!-- END LIST PARTNER OFFER OBJECT  -->
+
+          
         <!--
           <div class="d-flex justify-content-between">  
             <div class="form-check-label" for="flexCheckChecked">
@@ -117,22 +118,51 @@
               kakito_123 esta interesado en el siguiente objeto/s: 
           </div>
           <!-- LIST PARTNER OBJECT-->
+        <!--
           <div  class="d-flex align-content-stretch flex-wrap">
               <InventoryObject :showDeleteOption="true"  :showProductDetails="true" /> 
               <InventoryObjectEmpty   @click="showPartnerInventory=!showPartnerInventory; showMyInventory=false ;showStep2=false " />
           </div>
+        -->
           <!-- END LIST PARTNER OBJECT-->
 
-        
+          <!-- LIST MY OBJECT-->
+          <div  class="d-flex align-content-stretch flex-wrap">
+
+            <div v-for="obj in yourOfferObjects " :key="index" > 
+              <InventoryObject :object=obj  :showProductDetails="true"  class="m-1" @click="showPartnerObjectDetailed=true"/> 
+              <div  style="border-radius: 25px;" class="m-1 d-flex justify-content-center text-danger border border-1 border-danger" @click="removeFromObjectProposalList(obj)"> <i class="bi bi-x-lg "></i> </div>
+            </div>
+
+            <InventoryObjectEmpty   @click="showMyInventory=!showMyInventory; showMyInventory=false ; showStep2=false " />
+          </div>
+
           
           <div  style="font-size:16px "  class="m-2">
                 Y te ofrece los siguientes objetos
           </div>
-           <!-- LIST MY OBJECT  -->
-          <div  class="d-flex align-content-stretch flex-wrap">
+          <!-- LIST MY OBJECT  -->
+          <!--
+           <div  class="d-flex align-content-stretch flex-wrap">
               <InventoryObject  :showDeleteOption="true" :showProductDetails="true" /> 
               <InventoryObjectEmpty   @click="showMyInventory=!showMyInventory;showPartnerInventory=false ;showStep2=false " />
           </div>
+          -->
+
+
+          <!-- LIST MY OBJECT-->
+          <div  class="d-flex align-content-stretch flex-wrap">
+
+            <div v-for="obj in partnerOfferObjects" :key="index" > 
+              <InventoryObject :object=obj :showDeleteOption="true"  :showProductDetails="true"   @click="showPartnerObjectDetailed=true"/> 
+              <div class=" d-flex justify-content-center text-danger" @click="removeFromObjectProposalList(obj)"> <i class="bi bi-x-lg "></i> </div>
+            </div>
+
+            <InventoryObjectEmpty   @click="showPartnerInventory=!showPartnerInventory; showPartnerInventory=false ; showStep2=false " />
+          </div>
+
+
+
         <!--
           <div class="d-flex justify-content-between">  
             <div class="form-check-label" for="flexCheckChecked">
@@ -283,6 +313,24 @@
    
 
 
+<!-- Show Object Details-->
+<div v-if="showModalDetails" class="position-absolute top-0 start-10 bg-dark" >
+                <div class="" >
+                        <div class="d-flex justify-content-end"> 
+                        <i @click="showModalDetails=false" class="bi bi-x-lg display-1" ></i>
+                        </div>
+                        <InventoryObjectDetailed :showMakeOffer=false  :object=objectDetails  v-on:showMyInventory='showMyInventory=true'  v-on:closeModalObjectDetails="closeModalObjectDetails" :session_data="session_data" />
+                </div>
+                
+                <div class="m-4 pb-4" v-if="showModalDetails">
+                        <button v-if="session_data!=null && session_data.user!=null && !showMyInventory" @click="showExchangeProposal=true ; showModalDetails=false  " type="button" class="btn btn-primary">Hacer oferta por este producto</button>
+                        <button v-else  type="button" class="btn btn-secondary">Debe estar registrado para ofertar</button>
+                </div>        
+</div>
+
+
+
+
 
 
 
@@ -324,6 +372,9 @@ export default {
   data : function() {
       return {
 
+        showModalDetails:false ,
+        objectDetails : null ,
+
         showRejectProposal : false ,
         showAcceptProposal : false ,
         showAcceptProposalConfirmation : false ,
@@ -336,7 +387,35 @@ export default {
         showStep2 : false , 
         showStep3 : false , 
         showStep4 : false , 
-        }
+
+        yourOfferObjects  : [    {name:"myplay 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true }, 
+                            ],
+
+        partnerOfferObjects : [  {name:"AlguienPlay 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true }, 
+                            {name:"AlguienPlay 2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false}, 
+                            {name:"AlguienPlay 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true }, 
+                             ],
+
+        DBmyInventoryObjects :[{id:1 , name:"My inv PS 1", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:2 , name:"My inv PS 2", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:3 , name:"My inv PS 3", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:4 , name:"My inv 4", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:5 , name:"My inv 5", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:6 , name:"My inv 6", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:7 , name:"My inv 7", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                           ],
+        
+        DBPartnerInventoryObjects : [{id:1 ,name:"Partner Inv ps1", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:2 ,name:"Partner Inv 2", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:3 ,name:"Partner Inv 3", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:4 ,name:"Partner Inv 4", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:5 ,name:"Partner Inv 5", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:6 ,name:"Partner Inv 6", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                            {id:7 ,name:"Partner Inv 7", description : "Consola en buen estado con juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true }, 
+                           ],
+
+      
+      }
   },
   props: ['session_data'],
   emits: ['closeThisModal'],
