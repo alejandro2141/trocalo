b28aaecb497cfc0ba98b86f799e2a371f497fb80
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-28 19:17:15 -0300

    Update FilterForSearchView.vue

--- src/components/FilterForSearchView.vue
+++ src/components/FilterForSearchView.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="" style="font-size:30px">
-    <div class="text-primary ">
-        <text><i class="bi bi-controller"></i></text>
-        <text>| </text> 
-        <text><i class="bi bi-bicycle"></i></text>
-        <text>| </text>
-        
-        <text><i class="bi bi-tools"></i></text>
-        <text>| </text>
-        <text><i class="bi bi-music-player"></i></text>
-        <text>| </text>
+  <div class="m-0" style="font-size:30px ;width:350px">
+    <div class="text-secondary d-flex justify-content-between">
+        <text><i style="font-size:40px" class="bi bi-controller p-2"></i></text>
+       
+        <text><i style="font-size:40px" class="bi bi-bicycle p-2"></i></text>
+       
+    <!--     
+        <text><i  class="bi bi-wrench  p-2"></i></text>
+        <text> </text>
+    -->
+        <text><i class="bi bi-pc-display-horizontal p-2"></i></text>
+      
     <!--
         <text><i class="bi bi-house"></i></text>
         <text>| </text>
@@ -18,10 +19,11 @@
         <text><i class="bi bi-car-front"></i></text>
         <text>| </text>
     -->
-        <text>... </text> 
-
-        <input type="text" placeholder="Buscar"   id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-primary" required minlength="4" maxlength="20" size="7" />
-         
+        <div><i class="bi bi-list p-2 text-white"></i></div>
+        
+        <div>
+          <input type="text" placeholder="..."  style="text-align:center; font-size:15px" id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
+        </div>    
 
     </div>
     
