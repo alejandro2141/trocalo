a1d7d0825ca911b6250571557389b209fb5f87ab
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-28 19:17:06 -0300

    Update InventoryObjectDetailed.vue

--- src/components/InventoryObjectDetailed.vue
+++ src/components/InventoryObjectDetailed.vue
@@ -15,51 +15,67 @@
         </div>
     -->
         <div style="font-size:20px">
-            {{object.name}}
+          &nbsp;&nbsp;&nbsp;&nbsp;  {{object.name}}
         </div>
         <div style="font-size:16px">
-            {{object.owner}}
+            <i style="font-size:30px" class="bi bi-person-standing text-secondary"></i>   {{object.owner}}
         </div>
         <div class="bg-secondary" style="width:350px; height:300px">
-            IMAGE
+            <img class=""  style="width:350px; height:300px" src="" /> 
         </div>
         
         <div class="d-flex justify-content-start ">
-                <div class="bg-secondary m-1 " style="width:60px; height:60px">
-                    IMAGE
+                <div class="bg-secondary m-1 " style="width:80px; height:80px">
+                    <img class=""  style="width:80px; height:80px" src="" /> 
                 </div>
-                <div class="bg-secondary m-1" style="width:60px; height:60px">
-                    IMAGE
+                <div class="bg-secondary m-1" style="width:80px; height:80px">
+                    <img class=""  style="width:80px; height:80px" src="" /> 
                 </div>
-                <div class="bg-secondary m-1" style="width:60px; height:60px">
-                    IMAGE
+                <div class="bg-secondary m-1" style="width:80px; height:80px">
+                    <img class=""  style="width:80px; height:80px" src="" /> 
                 </div>
-                <div class="bg-secondary m-1" style="width:60px; height:60px">
-                    IMAGE
+                <div class="bg-secondary m-1" style="width:80px; height:80px">
+                    <img class=""  style="width:80px; height:80px" src="" /> 
                 </div>
         </div>
 
         <div >
             {{object.description}}
         </div>
-        <div class="mt-1" style="font-size:20px">
-           Cambiaria por:  
+        
+        <!--
+        <div class="mt-1" style="font-size:40px">
+            <i class="bi bi-shuffle text-secondary"></i>
         </div>
-        <div>
-            {{object.alt1}}<br>
-            {{object.alt2}}<br>
-            {{object.alt3}}<br>
+        -->
+
+        <div class="m-2">
+            <i style="font-size:20px" class="bi bi-shuffle text-secondary"></i>  {{object.alt1}}<br>
+            <i style="font-size:20px" class="bi bi-shuffle text-secondary"></i> {{object.alt2}}<br>
+            <i style="font-size:20px" class="bi bi-shuffle text-secondary"></i> {{object.alt3}}<br>
+            <i style="font-size:20px" class="bi bi-shuffle text-secondary"></i> Cambiaría por Otros Objetos
+                <text v-if="object.otherObj" style="font-size:25px" class="text-success">
+                        SI
+                </text>
+                <text v-else class="text-danger" style="font-size:25px" >
+                        NO
+                </text>
+
+            <br>
         </div>
+        <br>
+        <!-- 
         <div class="mt-1" style="font-size:20px">
            Cambiaria por otros objetos:  
-           <div v-if="object.otherObj">
-                SI
-           </div>
-           <div v-else >
-                NO
-           </div>
+           <text v-if="object.otherObj" class="text-success">
+                 SI
+           </text>
+           <text v-else class="text-danger" >
+                 NO
+           </text>
 
         </div>
+    -->
 
     </div> 
 
