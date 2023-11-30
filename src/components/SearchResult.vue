dda6234c79e847494d6c2f64e77e9c4e544c8be0
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-30 20:49:57 -0300

    Update SearchResult.vue

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,18 +1,29 @@
+<script setup>
+import InventoryObject from './InventoryObject.vue'
+import InventoryObjectDetailed from './InventoryObjectDetailed.vue'
+import FilterForSearchView from '../components/FilterForSearchView.vue'
+import InventoryList from '../components/InventoryList.vue' 
+import ExchangeProposal from '../components/ExchangeProposal.vue'
+</script>
+
 <template>
 
   <div  style="width:400px" class="m-0"   >
 
-
-    <div class="m-0 " v-if=" !showModalDetails && !showExchangeProposal ">
-        <FilterForSearchView  />
+    <div style="height:10px">
     </div>
 
+
+    <div class="m-0 " v-if=" !showModalDetails && !showExchangeProposal ">
+        <FilterForSearchView v-on:filterByText="filterByText" v-on:filterByCategory="filterByCategory" />
+    </div>
+<br>
     
     <!-- 1- SHOW PUBLIC OBJECTS ARE AVAILABLE FOR SEARCH-->
     <div v-if="showExchangeProposal==false && showModalDetails==false" >
         
-        <div class="d-flex flex-wrap"> 
-          <div v-for="obj in objects" :key="index" > 
+        <div v-if="objects_filtered !=null" class="d-flex flex-wrap"> 
+          <div v-for="obj in objects_filtered" :key="index" > 
             <InventoryObject @click="objectDetails=obj ;showModalDetails=true;" :object=obj  class="m-1"/>
           </div>
         </div>
@@ -77,20 +88,9 @@
 </style>
 
 
-<script lang="ts">
-
-import InventoryObject from './InventoryObject.vue'
-import InventoryObjectDetailed from './InventoryObjectDetailed.vue'
-import FilterForSearchView from '../components/FilterForSearchView.vue'
-import InventoryList from '../components/InventoryList.vue' 
-import ExchangeProposal from '../components/ExchangeProposal.vue'
-
+<script>
 export default {
 
-  components: {
-    InventoryObject,InventoryObjectDetailed,FilterForSearchView,InventoryList,ExchangeProposal
-  },
-  
   data : function() {
       return {
         showModalDetails: false,
@@ -100,16 +100,19 @@ export default {
         objectDetails : null, 
 
         //trae desde BKND
-        objects : [         {name:"AlguienPlay 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true }, 
-                            {name:"AlguienPlay 2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false}, 
-                            {name:"AlguienPlay 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true }, 
-                            {name:"AlguienPlay 4", description : "Consola en buen estado con 5 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_555" , otherObj: false}, 
-                            {name:"AlguienPlay 5", description : "Consola en buen estado con 6 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_6666", otherObj: true}, 
-                            {name:"AlguienPlay 6", description : "Consola en buen estado con 7 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_7777", otherObj: false}, 
-                            {name:"AlguienPlay 7", description : "Consola en buen estado con 8 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_8888", otherObj: true}, 
+                           
+        objects : [         {id:1 , name:"abcgame 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true , category_1: 1 ,  category_2: 2  }, 
+                            {id:2 , name:"bcdesport  2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false , category_1: 2  , category_2: 3 }, 
+                            {id:3 , name:"efghi 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true , category_1: 3  , category_2: 4}, 
+                            {id:4 , name:"abhijkl 4", description : "Consola en buen estado con 5 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_555" , otherObj: false, category_1: 4  , category_2: 5} , 
+                            {id:5 , name:"AlguienPlay 5", description : "Consola en buen estado con 6 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_6666", otherObj: true, category_1: 5  , category_2: 6}, 
+                            {id:6 , name:"AlguienPlay 6", description : "Consola en buen estado con 7 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_7777", otherObj: false, category_1: 6  , category_2: 7}, 
+                            {id:7 , name:"AlguienPlay 7", description : "Consola en buen estado con 8 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_8888", otherObj: true, category_1: 7  , category_2: 8}, 
                            ],
 
+        objects_filtered : null ,
 
+      
       }
   },
   props: ['session_data'],
@@ -117,9 +120,43 @@ export default {
 
 created() {
   console.log("APP CREATED")
+
+    this.objects_filtered=this.objects
+
     },
 
 methods: {
+  
+    filterByText(ftext)
+    {
+    console.log("Filter By Text in Search Result:"+ftext)
+        if(ftext!=null)
+        {
+        this.objects_filtered= this.objects_filtered.filter(element =>  element.name.toLowerCase().includes(ftext) );
+        }
+        else 
+        {
+        this.objects_filtered=this.objects
+        }
+    },
+
+    filterByCategory(categories)
+    {
+      console.log("Filter by category  in searchResults "+categories)
+      // this.categories.includes(val)
+      if (categories !=null && categories.length>0)
+      {  
+        this.objects_filtered=this.objects.filter(element =>  categories.includes(element.category_1) ); 
+      }
+      else 
+      {
+        this.objects_filtered=this.objects
+      }
+
+
+
+    },
+
     closeObjectAndMyInventoryModal()
     {
       console.log("Close inventory List")
