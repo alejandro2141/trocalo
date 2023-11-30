2afa399d970dd36e5def376d00e294d312d8aec5
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-11-30 20:50:01 -0300

    Update FilterForSearchView.vue

--- src/components/FilterForSearchView.vue
+++ src/components/FilterForSearchView.vue
@@ -1,29 +1,39 @@
+<script setup>
+</script>
+
 <template>
   <div class="m-0" style="font-size:30px ;width:350px">
-    <div class="text-secondary d-flex justify-content-between">
-        <text><i style="font-size:40px" class="bi bi-controller p-2"></i></text>
+
+    <div class="text-secondary ">
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(1) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(1)" > <i style="font-size:40px" class="bi bi-controller "></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(2) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(2)" > <i style="font-size:40px" class="bi bi-bicycle "></i> </text>
+        
+         <input type="text" placeholder="Busca Algo " v-model="textFilter" style="text-align:center; font-size:20px" id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
        
-        <text><i style="font-size:40px" class="bi bi-bicycle p-2"></i></text>
-       
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(3) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(3)" > <i style="font-size:40px" class="bi bi-lamp"></i> </text>
+          
     <!--     
-        <text><i  class="bi bi-wrench  p-2"></i></text>
+        
         <text> </text>
     -->
-        <text><i class="bi bi-pc-display-horizontal p-2"></i></text>
-      
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(4) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(4)" > <i style="font-size:40px" class="bi-pc-display-horizontal"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(5) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(5)" > <i style="font-size:40px" class="bi bi-book"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(6) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(6)" > <i style="font-size:40px" class="bi bi-car-front"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(7) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(7)" > <i style="font-size:40px" class="bi bi-wrench"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(8) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(8)" > <i style="font-size:40px" class="bi bi-sunglasses"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(9) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(9)" > <i style="font-size:40px" class="bi bi-suitcase2"></i> </text>
+        <text><i class="bi bi-list p-2 text-white"></i></text>
     <!--
         <text><i class="bi bi-house"></i></text>
         <text>| </text>
-        <text><i class="bi bi-book"></i></text>
+        
         <text>| </text>
-        <text><i class="bi bi-car-front"></i></text>
+        
         <text>| </text>
     -->
-        <div><i class="bi bi-list p-2 text-white"></i></div>
+     
         
-        <div>
-          <input type="text" placeholder="..."  style="text-align:center; font-size:15px" id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
-        </div>    
+         
 
     </div>
     
@@ -33,3 +43,67 @@
 <style scoped>
 
 </style>
+
+
+
+<script>
+export default {
+
+  data : function() {
+      return {
+        textFilter : null ,
+        categories : [] , 
+      }
+  },
+
+  props: [],
+  emits: ['filterByText','filterByCategory'],
+
+created() {
+    },
+
+methods: {
+
+    addCategoryToSearch(val)
+    {
+
+      if (this.categories.includes(val))
+      {
+        this.categories= this.categories.filter(element =>  element != val );
+      }
+      else 
+      {
+      this.categories.push(val)
+      } 
+
+      console.log("this.categories: "+this.categories)
+      this.$emit('filterByCategory',this.categories)
+    },
+  
+      
+
+    },
+
+watch : {
+
+ 
+    
+  textFilter(newVal, oldVal)
+    {
+      console.log("textFIlter change to "+newVal)
+      if (newVal !=null && newVal.length > 2)
+      {
+        this.$emit('filterByText',newVal)
+      }
+      else 
+      {
+        this.$emit('filterByText',null)
+      }
+
+    }
+
+      }
+}
+</script>
+
+
