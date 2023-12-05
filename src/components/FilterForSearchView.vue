b97634043bea67813664d8708e4687f66aa01c8d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-05 17:09:20 -0300

    Update FilterForSearchView.vue

--- src/components/FilterForSearchView.vue
+++ src/components/FilterForSearchView.vue
@@ -5,30 +5,46 @@
   <div class="m-0" style="font-size:30px ;width:350px">
 
     <div class="text-secondary ">
-        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(1) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(1)" > <i style="font-size:40px" class="bi bi-controller "></i> </text>
-        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(2) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(2)" > <i style="font-size:40px" class="bi bi-bicycle "></i> </text>
-        
-         <input type="text" placeholder="Busca Algo " v-model="textFilter" style="text-align:center; font-size:20px" id="searchText" name="searchText" class=" form-control-sm mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20" size="7" />
-       
-        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(3) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(3)" > <i style="font-size:40px" class="bi bi-lamp"></i> </text>
-          
+
+      <div class="d-flex justify-content-around">
+        <!-- 
+        <text style="border-radius: 30px;" class="border p-0" :class="[ categories.includes(1) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(1)" > <i style="font-size:40px" class="bi bi-controller "></i> </text>
+        -->
+        <input type="text" placeholder=" (  Buscar  ) " v-model="textFilter" style="text-align:center; font-size:23px ;border-radius:15px" id="searchText" name="searchText" class=" mt-1 bg-dark text-white border-0 p-0" required minlength="4" maxlength="20" size="7" />
+       <!-- 
+        <text style="border-radius: 30px;" class="border p-0" :class="[ categories.includes(2) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(2)" > <i style="font-size:40px" class="bi bi-bicycle "></i> </text>
+        -->
+           
+      </div>
     <!--     
         
         <text> </text>
     -->
+    <div class="d-flex justify-content-between">
+      <!-- 
+      <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(3) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(3)" > <i style="font-size:40px" class="bi bi-lamp"></i> </text>
+      -->
+      <text style="border-radius: 30px;" class="border p-0" :class="[ categories.includes(1) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(1)" > <i style="font-size:40px" class="bi bi-controller "></i> </text>
+        
         <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(4) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(4)" > <i style="font-size:40px" class="bi-pc-display-horizontal"></i> </text>
         <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(5) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(5)" > <i style="font-size:40px" class="bi bi-book"></i> </text>
+        <text style="border-radius: 30px;" class="border p-0" :class="[ categories.includes(2) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(2)" > <i style="font-size:40px" class="bi bi-bicycle "></i> </text>
+        
+        <!-- 
         <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(6) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(6)" > <i style="font-size:40px" class="bi bi-car-front"></i> </text>
-        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(7) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(7)" > <i style="font-size:40px" class="bi bi-wrench"></i> </text>
-        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(8) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(8)" > <i style="font-size:40px" class="bi bi-sunglasses"></i> </text>
-      <!--  <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(9) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(9)" > <i style="font-size:40px" class="bi bi-suitcase2"></i> </text>
       -->
+        
+      <!-- 
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(8) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(8)" > <i style="font-size:40px" class="bi bi-sunglasses"></i> </text> 
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(9) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(9)" > <i style="font-size:40px" class="bi bi-suitcase2"></i> </text>
+        <text style="border-radius: 30px;" class="border p-2" :class="[ categories.includes(7) ? 'border-secondary': 'border-dark' ]"  @click="addCategoryToSearch(7)" > <i style="font-size:40px" class="bi bi-wrench"></i> </text> -->
       <!-- 
         <text><i class="bi bi-list p-2 text-white"></i></text>
       -->
 
-        <text><i class="bi bi-arrow-return-left text-white"></i></text>
-    <!--
+        <text style="border-radius: 30px;"  ><i class="bi bi-arrow-return-left p-2 text-white"></i></text>
+    </div>
+        <!--
         <text><i class="bi bi-house"></i></text>
         <text>| </text>
         
