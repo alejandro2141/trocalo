3b9e4511f030000f553cbef70850729065dadb96
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-05 17:09:10 -0300

    Update NewObjectForm.vue

--- src/components/NewObjectForm.vue
+++ src/components/NewObjectForm.vue
@@ -13,30 +13,46 @@ import InventoryList from '../components/InventoryList.vue'
   <div v-if="step1">
 
     <div>
-      <input type="text" placeholder="Titulo"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark    text-white border-white" required minlength="4" maxlength="20"  />
+      <input type="text" placeholder="Titulo"  v-model="input_name" id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark    text-white border-white" required minlength="4" maxlength="40"  />
     </div>
 
     <div class="d-flex flex-wrap w-100">
 
-      <div style="width:150px; height:150px" class="bg-white m-1 ">
+      <div @click="$refs.show_uploadPicture_1_input.click()" style="width:200px; height:200px ; border-radius:15px" class="bg-white m-1 text-center">
+          <i v-if="!show_uploadPicture_1_preview" style="font-size:100px" class="bi bi-camera text-secondary pt-3"></i>
+          <img v-else   :src='previewImage_1' class="uploading-image" style="width:200px; height:200px ; border-radius:15px"  />   
+          <input ref="show_uploadPicture_1_input" type="file" accept="image/jpeg" @change='uploadImage_1' style="display: none">
       </div>
 
-      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      <div @click="$refs.show_uploadPicture_2_input.click()" style="width:100px; height:100px ; border-radius:15px" class="bg-white m-1 text-center">
+          <i v-if="!show_uploadPicture_2_preview" style="font-size:50px" class="bi bi-camera text-secondary pt-3"></i>
+          <img v-else   :src='previewImage_2' class="uploading-image" style="width:100px; height:100px ; border-radius:15px"  />   
+          <input ref="show_uploadPicture_2_input" type="file" accept="image/jpeg" @change='uploadImage_2' style="display: none">
       </div>
 
-      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      <div @click="$refs.show_uploadPicture_3_input.click()" style="width:100px; height:100px ; border-radius:15px" class="bg-white m-1 text-center">
+          <i v-if="!show_uploadPicture_3_preview" style="font-size:50px" class="bi bi-camera text-secondary pt-3"></i>
+          <img v-else   :src='previewImage_3' class="uploading-image" style="width:100px; height:100px ; border-radius:15px"  />   
+          <input ref="show_uploadPicture_3_input" type="file" accept="image/jpeg" @change='uploadImage_3' style="display: none">
       </div>
 
-      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      <div @click="$refs.show_uploadPicture_4_input.click()" style="width:100px; height:100px ; border-radius:15px" class="bg-white m-1 text-center">
+          <i v-if="!show_uploadPicture_4_preview" style="font-size:50px" class="bi bi-camera text-secondary pt-3"></i>
+          <img v-else   :src='previewImage_4' class="uploading-image" style="width:100px; height:100px ; border-radius:15px"  />   
+          <input ref="show_uploadPicture_4_input" type="file" accept="image/jpeg" @change='uploadImage_4' style="display: none">
       </div>
 
-      <div style="width:100px; height:100px" class="bg-white m-1 ">
+      <div @click="$refs.show_uploadPicture_5_input.click()" style="width:100px; height:100px ; border-radius:15px" class="bg-white m-1 text-center">
+          <i v-if="!show_uploadPicture_5_preview" style="font-size:50px" class="bi bi-camera text-secondary pt-3"></i>
+          <img v-else   :src='previewImage_5' class="uploading-image" style="width:100px; height:100px ; border-radius:15px"  />   
+          <input ref="show_uploadPicture_5_input" type="file" accept="image/jpeg" @change='uploadImage_5' style="display: none">
       </div>
 
+      
     </div>
 
     <div>
-      <textarea  placeholder="Descripcion del objeto" class="form-control-lg mt-1 bg-dark text-white border-white" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <textarea  v-model="input_description" maxlength="100"  placeholder="Descripcion del objeto" class="form-control-lg mt-1 bg-dark text-white border-white" id="exampleFormControlTextarea1" rows="3"></textarea>
     </div>
     
     <br>
@@ -45,26 +61,27 @@ import InventoryList from '../components/InventoryList.vue'
           Cambiaria Por: 
         </p>
         <div>
-          <input type="text" placeholder="Robot asesino Gigante"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20"  />
+          <input v-model="input_exchange_option1" type="text" placeholder="Robot asesino Gigante"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="30"  />
         </div>
         <div>
-          <input type="text" placeholder="Delorean polarizado"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20"  />
+          <input v-model="input_exchange_option2" type="text" placeholder="Delorean polarizado"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="30"  />
         </div>
         <div>
-          <input type="text" placeholder="Ferrary 4fwd"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="20"  />
+          <input v-model="input_exchange_option3" type="text" placeholder="Ferrary 4fwd"   id="searchText" name="searchText" class="form-control-lg mt-1 bg-dark text-white border-white" required minlength="4" maxlength="30"  />
         </div>
     </div>
     
     <br>
     <div>
         <p style="font-size:20px">
-         Cambiaria por otros Objetos? <br> <button type="button" class="btn btn-dark border-white">NO</button>  <button type="button" class="btn btn-success">SI</button>
+         Cambiaria por otros Objetos? <br> <button @click="input_exchange_other=false"  type="button" :class="[!input_exchange_other ? 'btn btn-success' : 'btn btn-dark']"  >NO</button>  <button  @click="input_exchange_other=true" type="button" :class="[input_exchange_other ? 'btn btn-success' : 'btn btn-dark']" >SI</button>
         </p>
     </div>
 
     <br>
-    <div class="d-flex justify-content-center display-1 text-success w-100 bg-dark p-3 " >
-        <i @click="step1=false;showRequestConfirmation=true" class="bi bi-cloud-upload"></i>
+    <div style="font-size:20px" class=" text-success w-100 bg-dark p-3 text-center" >
+       <text> Guardar en mi inventario </text> <br>
+       <i @click="step1=false;showRequestConfirmation=true" class="bi bi-cloud-arrow-up"></i> 
     </div>
     
 
@@ -75,17 +92,19 @@ import InventoryList from '../components/InventoryList.vue'
      <div class="position-absolute top-0  text-center start-0 bg-dark w-100 h-100" >
         <div>
             <!-- SPACE FILLER -->
-            <p style="height:300px">
+            <p style="height:200px">
             </p>
             Articulo Guardado con Exito en tu Inventario
         </div>
       <br>
+
+
         <div class="d-flex justify-content-center">
-           <div class="fixed-bottom display-1 text-success w-100 bg-dark p-3 ">  
+           <div class=" text-success w-100 bg-dark p-3 ">  
               <div class="d-flex justify-content-center">
 
-                <a href="/">
-                <i class="text-success bi bi-house" style="font-size:40px "></i> 
+                <a style="font-size:25px" @click="$emit('closeNewObjectForm')" >
+                  <i class="bi bi-arrow-left-square"></i>  Regresar al Inventario
                 </a>
               
               </div>
@@ -133,16 +152,93 @@ export default {
       return {
         step1 : true ,
         showRequestConfirmation: false,
+        input_exchange_option1 : null ,
+        input_exchange_option2 : null ,
+        input_exchange_option3 : null ,
+        input_exchange_other : true ,
+
+        input_name : null, 
+        input_description : null ,
+
+        previewImage_1:null,
+        previewImage_2:null,
+        previewImage_3:null,
+        previewImage_4:null,
+        previewImage_5:null,
+        show_uploadPicture_1_preview : false , 
+        show_uploadPicture_2_preview : false , 
+        show_uploadPicture_3_preview : false , 
+        show_uploadPicture_4_preview : false , 
+        show_uploadPicture_5_preview : false , 
+
 
       }
   },
   props: ['session_data'],
-  emits: [],
+  emits: ['closeNewObjectForm'],
 
 created() {
     },
 
 methods: {
+
+ //  IMAGE 1 
+ /*   
+ show_uploadPicture_1()
+    {
+        this.$refs.show_uploadPicture_1_input.click()
+    },
+    */
+    uploadImage_1(e)
+    {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{ this.previewImage_1 = e.target.result;};
+    this.show_uploadPicture_1_preview=true
+    },
+
+ //  IMAGE 2 
+    uploadImage_2(e)
+    {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{ this.previewImage_2 = e.target.result;};
+    this.show_uploadPicture_2_preview=true
+    },
+
+//  IMAGE 3
+    uploadImage_3(e)
+    {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{ this.previewImage_3 = e.target.result;};
+    this.show_uploadPicture_3_preview=true
+    },
+
+ //  IMAGE 4 
+    uploadImage_4(e)
+    {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{ this.previewImage_4 = e.target.result;};
+    this.show_uploadPicture_4_preview=true
+    },
+ //  IMAGE 5 
+    uploadImage_5(e)
+    {
+      const image = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{ this.previewImage_5 = e.target.result;};
+    this.show_uploadPicture_5_preview=true
+    },
+
+
+
     },
 
 watch : {
