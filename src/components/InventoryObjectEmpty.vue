197945c107db82031225fe2d3028646dd43de6bc
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-05 17:09:13 -0300

    Update InventoryObjectEmpty.vue

--- src/components/InventoryObjectEmpty.vue
+++ src/components/InventoryObjectEmpty.vue
@@ -3,23 +3,71 @@
 
 <template>
 
-
-<div class="d-flex justify-content-center w-100" >
-         <i class="display-1 bi bi-plus-lg"></i>
-</div>
+  <div class=""    >
 
     
+    <div v-if="horizontal"> 
+        <div  @click="selectObject=!selectObject"  class="border border-2 rounded" :class="[selectObject ? 'border-white' : 'border-dark' , '' ]"  style="width:100px ; border-style: dotted"   >
 
-<!-- 
-    <div class="" style="width:100px; height:100px;  display: flex; justify-content: center;  align-items: center; border-style: dashed;">
-        
-        
+                <div class="bg-white"  style="display: grid;align-items: center;width:100px; height:100px">
+                   +
+                </div>
 
-            <i class="display-1 bi bi-plus-lg"></i>
-        
+                <div>
+                    <div>
+                   
+                    </div>
+                    <div v-if="showProductDetails" class="text-secondary">
+                       
+                    </div>
+                    <br>
+                    <div v-if="showProductPreference">
+                       
+                    </div>
+                </div>
+        </div>
     </div> 
--->
 
+    <div  v-else-if="horizontal_short"> 
+        <div  @click="selectObject=!selectObject"  class="d-flex justify-content-start border border-2 rounded" :class="[selectObject ? 'border-white' : 'border-dark' , '' ]"  style=" border-style: dotted"   >
+
+                <div class="bg-white" style="width:100px; height:100px" >
+                    <img class=""  style="width:100px; height:100px" src="" /> 
+                </div>
+
+                <div class="m-1">
+
+                    <div>
+                   
+                    </div>
+                    <div v-if="showProductDetails" class="text-secondary">
+                       
+                    </div>
+
+                </div>
+
+        </div>
+    </div> 
+
+    <div v-else class="" >
+        <div  @click="selectObject=!selectObject"   style="width:100px ;" >
+                <div class="bg-dark w-100 border border-2 rounded text-center "   :class="[selectObject ? 'border-white' : 'border-secondary' , '' ]"   style="width:100px; height:100px;align-items: center;display: grid;	align-items: center;">
+                    <i style="font-size:40px" class="bi bi-plus-lg "></i>
+                </div>
+               
+                <div  class="d-flex align-items-center m-0">
+                   <br>
+                   <br>
+                   <br>
+                   <br>
+                </div>
+        </div>
+
+    </div>
+
+
+
+  </div>
 
 </template>
 
@@ -37,10 +85,11 @@ export default {
     data : function() {
         return {
         lala : true ,
+        selectObject : false ,
         }
     },
 
-    props: ['showProductDetails','showProductPreference' ],
+    props: ['showProductDetails','horizontal_short','horizontal' ,'object' ],
     emits: [],
 
 	created() {
@@ -54,6 +103,12 @@ export default {
       },
 
   watch : {
+
+            isActive(newval)
+            {
+                console.log("selected "+newval);
+            },
+
         }
 }
 </script>
