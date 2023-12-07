90c6974a75a1dc9c11136eeef0ffb343e237e4c9
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-07 20:03:01 -0300

    Create OfSentItem.vue

---
+++ src/components/OfSentItem.vue
@@ -0,0 +1,69 @@
+
+<template>
+ 
+ <div>
+  
+   
+
+    <div class="border border-1 rounded" :class="{'text-success border-success': accepted }"   >
+        <div class="d-flex justify-content-between">
+            <div>      
+            Cakito_123   <br>
+            Play Station 3 con 4 juegos.
+            </div>
+            
+            <div style="font-size:20px" :class="{'text-success': accepted }"  >
+                $17.000
+            </div>
+
+            <div>
+                <i style="font-size:30px" :class="{'text-success': accepted }" class="bi bi-cloud-upload p-2"></i>
+            </div>
+        </div>
+    </div>
+
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import InventoryObject from '../components/InventoryObject.vue'
+import InventoryObjectDetailed from '../components/InventoryObjectDetailed.vue'
+import InventoryList from '../components/InventoryList.vue' 
+
+
+
+export default {
+
+
+  components: {
+    InventoryObject,InventoryObjectDetailed,InventoryList
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data','accepted'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
