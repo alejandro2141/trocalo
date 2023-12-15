89b5fd86afcbc67e47e886b4069f3eca7abf6de0
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-15 18:49:54 -0300

    Create ViewSearch.vue

---
+++ src/views/ViewSearch.vue
@@ -0,0 +1,44 @@
+
+<template>
+
+  <SearchResult :session_data="session_data" />
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import SearchResult from '../components/SearchResult.vue'
+
+
+export default {
+
+  components: {
+      SearchResult
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data'],
+  emits: ['sessionCreated'],
+
+created() {
+  console.log("APP CREATED")
+    },
+
+methods: {
+    },
+
+watch : {
+      }
+}
+</script>
+
+
