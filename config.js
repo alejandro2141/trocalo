72fdf3c789a0cca7dab5e0bcb33d6a01c1858414
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-18 20:32:51 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+
+/*
+let BKND_CONFIG ;
+let BKND_REGIONS = [] ;
+*/
+let BKND_CONFIG = {
+    //BKND_HOST : "HTTP://ec2-3-143-168-51.us-east-2.compute.amazonaws.com:8080",
+    //local ENv
+    BKND_HOST : "http://localhost:8080",
+    //DEVEL
+    //BKND_HOST : "https://www.horapo.com:8443",
+    //PROD
+    //BKND_HOST : "https://www.horapo.com:8443",
+    BKND_TIME : 1000,
+    BNKD_USER : 'abc'
+  };
+export { BKND_CONFIG } ;
+
