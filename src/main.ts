b1a42ac841602becf3c8c40c8a17e878ce7150b5
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-18 18:25:31 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App)
 
 app.use(createPinia())
