93a17dcb90295bdd23bcbf672119b63f01b9f6ae
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-01-05 20:19:13 -0300

    Update ViewProposalsReceived.vue

--- src/views/ViewProposalsReceived.vue
+++ src/views/ViewProposalsReceived.vue
@@ -7,6 +7,8 @@ import ProposalReceivedEndedDetails from   '../components/ProposalReceivedEndedD
 /*import OfReceivedItemList from '../components/OfReceivedItemList.vue'*/
 import InventoryList  from     '../components/InventoryList.vue'
 
+import { BKND_CONFIG } from '../../config.js'
+import axios from 'axios'
 </script>
 
 
@@ -22,7 +24,7 @@ import InventoryList  from     '../components/InventoryList.vue'
     <p class="text-white text-center" style="font-size:20px">Recibidas </p>
     <p class="text-secondary" style="font-size:12px" >A la espera  que apruebes o rechaces las propuestas </p>
     <div v-for="of in ofReceived"  > 
-        <ProposalReceived class="m-1" @click="showExchangeProposalReceived=true"  :offer='of'  />
+        <ProposalReceived class="m-1" @click="ofSelected=of ; showExchangeProposalReceived=true"  :offer='of'  />
     </div>
 <!-- OF SENT -->
 
@@ -32,7 +34,7 @@ import InventoryList  from     '../components/InventoryList.vue'
     <p class=" text-center" style="font-size:16px">Aceptadas </p>
     <p class="text-secondary" style="font-size:12px" >Debes pagar monto indicado de transporte y despacho para completar el intercambio de objetos </p>
     <div v-for="of in ofAccepted"  > 
-        <ProposalReceived  class="m-1" :accepted='true' @click="showExchangeProposalReceivedAccepted=true"  :offer='of'  />
+        <ProposalReceived  class="m-1" :accepted='true' @click="ofSelected=of ;showExchangeProposalReceivedAccepted=true"  :offer='of'  />
     </div>
 <!-- OF SENT ACCEPTED -->
   
@@ -42,22 +44,24 @@ import InventoryList  from     '../components/InventoryList.vue'
     <p class="text-secondary text-center" style="font-size:16px">Finalizadas </p>
     <div class="text-secondary">
       <div v-for="of in ofEnded"  > 
-        <ProposalReceived  class="m-1" @click="showExchangeProposalReceivedEnded=true"  :offer='of'  />
+        <ProposalReceived  class="m-1" @click="ofSelected=of ;showExchangeProposalReceivedEnded=true"  :offer='of'  />
       </div>
     </div>
 <!-- OF SENT ENDED -->
 
-  
+
+
+<!-- OF DETAILED  -->  
     <div  v-if="showExchangeProposalReceived"  class="position-absolute top-0 start-0 bg-dark w-100 d-flex justify-content-center" >
-        <ProposalReceivedDetails from  v-on:closeModal="closeModal()" />
+        <ProposalReceivedDetails :offer='ofSelected' :session_data="session_data" v-on:closeModal="closeModal()" />
     </div>
 
     <div  v-if="showExchangeProposalReceivedAccepted"  class="position-absolute top-0 start-0 bg-dark w-100 d-flex justify-content-center" >
-        <ProposalReceivedAcceptedDetails from  v-on:closeModal="closeModal()" />
+        <ProposalReceivedAcceptedDetails :offer='ofSelected' :session_data="session_data"   v-on:closeModal="closeModal()" />
     </div>
 
     <div  v-if="showExchangeProposalReceivedEnded"  class="position-absolute top-0 start-0 bg-dark w-100 d-flex justify-content-center" >
-        <ProposalReceivedEndedDetails from  v-on:closeModal="closeModal()" />
+        <ProposalReceivedEndedDetails :offer='ofSelected' :session_data="session_data"   v-on:closeModal="closeModal()" />
     </div>
 
 <!--
@@ -104,30 +108,13 @@ export default {
         showExchangeProposalReceivedEnded : false ,
 
 
-        ofReceived : [  
-                            {id:1 , name:"abcgame 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true , category_1: 1 ,  category_2: 2 ,  amount: '17990'  , status: 5  }, 
-                            {id:2 , name:"bcdesport  2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false , category_1: 2  , category_2: 3 ,  amount: '17990' , status: 5 }, 
-                            {id:3 , name:"efghi 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true , category_1: 3  , category_2: 4,  amount: '17990' , status: 5 }, 
-                            {id:4 , name:"abhijkl 4", description : "Consola en buen estado con 5 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_555" , otherObj: false, category_1: 4  , category_2: 5,  amount: '17990' , status:5  } , 
-                            {id:5 , name:"AlguienPlay 5", description : "Consola en buen estado con 6 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_6666", otherObj: true, category_1: 5  , category_2: 6,  amount: '17990' , status: 5  }, 
-                       
-                        ],
+        ofReceived : [],
         
-        ofAccepted : [  
-                            {id:1 , name:"abcgame 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true , category_1: 1 ,  category_2: 2  ,  amount: '17990' , status: 2  }, 
-                            {id:2 , name:"bcdesport  2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false , category_1: 2  , category_2: 3,  amount: '17990' , status: 2   }, 
-                            {id:3 , name:"efghi 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true , category_1: 3  , category_2: 4,  amount: '17990' , status: 2  }, 
-                            {id:4 , name:"abhijkl 4", description : "Consola en buen estado con 5 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_555" , otherObj: false, category_1: 4  , category_2: 5,  amount: '17990' , status: 2  } , 
-                            {id:5 , name:"AlguienPlay 5", description : "Consola en buen estado con 6 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_6666", otherObj: true, category_1: 5  , category_2: 6,  amount: '17990' , status: 2  }, 
-                          ],
+        ofAccepted : [],
         
-        ofEnded : [  
-                            {id:1 , name:"abcgame 1", description : "Consola en buen estado con 2 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_222"  , otherObj: true , category_1: 1 ,  category_2: 2 ,  amount: '17990' , status: 3   }, 
-                            {id:2 , name:"bcdesport  2", description : "Consola en buen estado con 3 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito_333"  , otherObj: false , category_1: 2  , category_2: 3 ,  amount: '17990' , status: 4  }, 
-                            {id:3 , name:"efghi 3", description : "Consola en buen estado con 4 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_444" , otherObj: true , category_1: 3  , category_2: 4,  amount: '17990' , status: 4  }, 
-                            {id:4 , name:"abhijkl 4", description : "Consola en buen estado con 5 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_555" , otherObj: false, category_1: 4  , category_2: 5 ,  amount: '17990' , status: 3  } , 
-                            {id:5 , name:"AlguienPlay 5", description : "Consola en buen estado con 6 juegos" , alt1:"Bicicleta"  , alt2:"X BOX"  , alt3:"Maquina de cortar pasto" , otherProducts: true , owner: "Kaquito1_6666", otherObj: true, category_1: 5  , category_2: 6,  amount: '17990' , status: 4  }, 
-                        ],
+        ofEnded : [],
+
+        ofSelected : null ,
 
       }
   },
@@ -135,22 +122,27 @@ export default {
   emits: [],
 
 created() {
+      this.getProposalsReceived()
     },
 
 methods: {
 
-    async getProposalsReceived(searchParams)
+    async getProposalsReceived()
     {
-    let params= {};
+      let response_json = await axios.post(BKND_CONFIG.BKND_HOST+"/private_get_proposals_received", this.session_data);
+      console.log("/private_get_proposals_received Response:"+JSON.stringify(response_json.data))
+      //this.inventoryObjects = jsonResponse.data
+      //this.inventory_objects_filtered=this.inventoryObjects
 
-    let jsonResponse = await axios.post(BKND_CONFIG.BKND_HOST+"/private_get_proposals_received", this.session_data);
-    console.log("/private_get_proposals_received Response:"+JSON.stringify(jsonResponse.data))
-    
-    //this.inventoryObjects = jsonResponse.data
-    //this.inventory_objects_filtered=this.inventoryObjects
+      let proposals = response_json.data
+
+      //filter 
+      this.ofReceived = proposals.filter(item => item.status ==  1).sort((a, b) => (a.id > b.id) ? 1 : -1);
+      this.ofAccepted = proposals.filter(item => item.status ==  100).sort((a, b) => (a.id > b.id) ? 1 : -1);
+      this.ofEnded = proposals.filter(item => item.status ==  200).sort((a, b) => (a.id > b.id) ? 1 : -1);
+
 
     },
-
     
     closeModal()
     {
