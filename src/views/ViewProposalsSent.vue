8aae6ec8a4ff18cd9272ba4104fc3177e95016bf
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-01-08 19:32:45 -0300

    Update ViewProposalsSent.vue

--- src/views/ViewProposalsSent.vue
+++ src/views/ViewProposalsSent.vue
@@ -136,6 +136,7 @@ methods: {
 
     closeModal()
     {
+      this.getProposalsSent()
       this.showExchangeProposalSent=false
       this.showExchangeProposalSentAccepted=false
       this.showExchangeProposalSentEnded=false
