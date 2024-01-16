bf56f683ca10a7498f18caeb3ce95a11f2b3a2fe
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-01-16 19:04:37 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
           <RouterLink style="" class='text-decoration-none'  @click="setUnderline(7)"  to="/ViewSearch"> 
             <i style="color:#EEEEEE ; font-size : 35px ;" class="bi bi-recycle"></i> <text style="color:#EEEEEE ; font-size : 35px ;">REUSAR.CL</text>
-
           </RouterLink>
 
           <RouterLink  class="" :class="{'text-decoration-underline' :underline_login}" @click="setUnderline(3)"  to="/ViewLogin"> 
@@ -37,15 +36,7 @@ import { RouterLink, RouterView } from 'vue-router'
           </div>
           
           <RouterLink  style="font-size : 20px ; border-radius: 10px;"  class="p-0 text-secondary"  :class="[ underline_search ? ' text-white' : 'bg-dark' ]"  @click="setUnderline(1)"   to="/ViewSearch">  
-            
-            <div style="background-image: url('/public/Logo.png') ;  height: 150px; width: 200px;  background-repeat: no-repeat, no-repeat;" >
-
-             
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-recycle" viewBox="0 0 16 16">
-                  <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.5.5 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244z"/>
-                </svg>
-
-            </div>
+            <i style="color:#EEEEEE ; font-size : 35px ;" class="bi bi-recycle"></i> <text style="color:#EEEEEE ; font-size : 35px ;">REUSAR.CL</text>
           </RouterLink>    
 
 
