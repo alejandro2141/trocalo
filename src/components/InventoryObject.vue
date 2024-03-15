6a384365819a9ec18c3db2ec8ac6ec9c7a5c70ba
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-03-15 18:52:36 -0300

    Update InventoryObject.vue

--- src/components/InventoryObject.vue
+++ src/components/InventoryObject.vue
@@ -11,8 +11,7 @@ import { PATH_PRODUCT_IMG } from '../../config.js'
         <div  @click="selectObject=!selectObject"  class="border border-2 rounded" :class="[selectObject ? 'border-white' : 'border-dark' , '' ]"  style="width:100px ; border-style: dotted"   >
 
                 <div class="bg-white"  style="width:100px; height:100px">
-               
-                    <img  style="width:100px; height:100px" :src="PATH_PRODUCT_IMG+'/'+object.img_ref1" /> 
+                   <img  style="width:100px; height:100px" :src="PATH_PRODUCT_IMG+'/'+object.img_ref1" /> 
                 </div>
 
                 <div>
@@ -53,15 +52,15 @@ import { PATH_PRODUCT_IMG } from '../../config.js'
         </div>
     </div> 
 
+    <!-- INVENTORY OBJECT  -->
     <div v-else class="" >
-        <div v-if="object!=null" @click="selectObject=!selectObject"  class="border border-2 rounded" :class="[selectObject ? 'border-white' : 'border-dark' , '' ]"  style="width:100px ; border-style: dotted" >
-                <div class="bg-white"  style="width:100px; height:100px">
-                   <!--  <img class=""  style="width:100px; height:100px" :src="PATH_PRODUCT_IMG+'/'+object.img_ref1" />  -->
-                    <div>
+        <div v-if="object!=null" @click="selectObject=!selectObject"  class="border border-2 rounded" :class="[(selectObject && !object.blocked_due_proposal_accepted) ? 'border-white' : 'border-dark' , '' ]"  style="width:100px ; border-style: dotted" >
+                <div class="bg-white image-container"  style="width:100px; height:100px">
+                    <div class="image-container img">
                         <img class="" style="width:100px; height:100px" :src="PATH_PRODUCT_IMG+'/'+object.img_ref1" />
                     </div>
-                    <div class="relative" style="top: 0px; left: 0px;" >
-                        <i v-if="object.blocked_due_proposal_accepted" style="font-size:40px" class="bi bi-unlock text-secondary"></i>
+                    <div v-if="object.blocked_due_proposal_accepted" class="overlay-text"  >
+                        <i  style="font-size:40px" class="bi bi-unlock text-white"></i>
                     </div>
                 </div>
                
@@ -70,7 +69,6 @@ import { PATH_PRODUCT_IMG } from '../../config.js'
                  <!-- {{object.description}} -->
                 </div>
         </div>
-
     </div>
 
 
@@ -80,6 +78,33 @@ import { PATH_PRODUCT_IMG } from '../../config.js'
 </template>
 
 <style scoped>
+
+.image-container {
+  position: relative;
+  display: inline-block;
+}
+
+.image-container img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay-text {
+  width:95px; 
+  height:95px ;
+  position: absolute;
+  top: 50%; 
+  left: 50%; 
+  transform: translate(-50%, -50%); 
+  background-color: #000000;  
+  opacity:0.5;
+  color: #fff; 
+  padding: 0px 0px; 
+  font-size: 20px; 
+  text-align: center;
+}
+
 
 </style>
 
