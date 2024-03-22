fc73fd9203236ed11819c82dfcdeaece0e91155d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-03-22 19:26:41 -0300

    Create ProposalReceivedCancelled.vue

---
+++ src/components/ProposalReceivedCancelled.vue
@@ -0,0 +1,127 @@
+
+<template>
+ 
+ <div>
+  
+    <div class="border border-1 rounded text-white border-secondary" >
+
+        <div class="d-flex justify-content-between">
+            <div class="m-1" >
+                 Has rechazado la propuesta de: <b> {{offer.source_owner_name}}</b><br>
+                 {{offer.title}} 
+            </div>
+            <div>
+               
+            </div>
+        </div>
+
+    </div>
+
+ </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+import InventoryObject from '../components/InventoryObject.vue'
+import InventoryObjectDetailed from '../components/InventoryObjectDetailed.vue'
+import InventoryList from '../components/InventoryList.vue' 
+
+
+
+export default {
+
+
+  components: {
+    InventoryObject,InventoryObjectDetailed,InventoryList
+  },
+  
+  data : function() {
+      return {
+
+      }
+  },
+  props: ['session_data','accepted','offer'],
+  emits: [],
+
+created() {
+    },
+
+methods: {
+
+    amountFormatter(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    //return "aaaaa"
+    },
+    
+    isNewProposal()
+    {
+        let creationDate = new Date(this.offer.timestamp)
+        let cdate = new Date()
+
+        if ( (cdate.getTime() - creationDate.getTime()) < (1000 * 60 * 60 * 24))
+        {
+            return true 
+        }
+        else 
+        {
+            return false 
+        }
+    },
+
+    evaluateRemainingDays(timestamp)
+    {
+        let creationDate = new Date(timestamp)
+        let cdate = new Date()
+
+        let days_passed = ( cdate.getTime() - creationDate.getTime() ) / (1000 * 60 * 60 * 24)  ;
+        let days_remaining =   this.offer.proposal_days - days_passed
+        return Math.floor(days_remaining)
+    },
+
+    evaluateRemainingDaysToPay(timestamp)
+    {
+        let creationDate = new Date(timestamp)
+        let cdate = new Date()
+
+        let days_passed = ( cdate.getTime() - creationDate.getTime() ) / (1000 * 60 * 60 * 24)  ;
+        let days_remaining =   3 - days_passed
+        return Math.floor(days_remaining)
+    },
+
+    evaluateOfferStatus(val)
+    {
+       
+        switch (val) {
+                case '1':
+                    return ('Enviada')
+                    break;
+                case '100':
+                    return ('Aceptada')
+                    break;
+                case '200':
+                    return ('Rechazada')
+                    break;
+                case '4':
+                    return ('Expirada')
+                    break;
+                default:
+                    return ('Failed Status')
+                    break;
+                }
+    }
+
+
+    },
+
+watch : {
+      }
+}
+</script>
+
+
