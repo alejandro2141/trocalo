b2daa03a17fe1082beeb1ea86b43e26e678b9f19
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-04-18 11:15:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,9 @@ let BKND_REGIONS = [] ;
 let BKND_CONFIG = {
     //BKND_HOST : "HTTP://ec2-3-143-168-51.us-east-2.compute.amazonaws.com:8080",
     //local ENv
-    BKND_HOST : "http://localhost:8080",
-//    BKND_HOST : "http://ec2-3-149-255-22.us-east-2.compute.amazonaws.com:8080",
+   // BKND_HOST : "http://localhost:8080",
+    BKND_HOST : "http://ec2-3-149-255-22.us-east-2.compute.amazonaws.com:8080",
+    
     //DEVEL
     //BKND_HOST : "https://www.horapo.com:8443",
     //PROD
