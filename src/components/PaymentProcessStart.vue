2362b0e16416fe4ad994b77ff89a18a5f4e59197
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-06-25 22:07:29 -0400

    Update PaymentProcessStart.vue

--- src/components/PaymentProcessStart.vue
+++ src/components/PaymentProcessStart.vue
@@ -15,15 +15,21 @@ import axios from 'axios'
       <i  @click="$emit('close')" class="display-1 bi bi-x-lg"></i>      
     </div>
 
-    Iniciamos el proceso de Pago Scanea el codigo QR o algo lindo o datos de transferencia
+    Actualmente contamos con las siguientes formas de pago. 
 
 
     <br>
     <br>
     <br>
-    <div class="border border-2 p-4 w-50 text-center" @click="showTransferData=!showTransferData"> Transferencia </div>
-    
+
+    <div class="d-flex justify-content-center">
+      <div class="border border-2 p-4 w-50 text-center" @click="showTransferData=!showTransferData"> Transferencia </div>
+    </div>
+
     <div v-if="showTransferData==true"> 
+      Debe realizar una transaccion a la siguiente cuenta:  
+      <br>
+
        Nombre:  Juan Morales<br>
        Rut:  13.909.371-2<br>
        Banco:  Banco de Chile<br>
@@ -31,22 +37,22 @@ import axios from 'axios'
        Numero Cta: 9626000<br>
        <br>
        <text>Importante.</text> <br>
-       Debe indicar el numero #<b>  {{offer.id}} </b> en el mensaje. 
+       Debe indicar el numero de transacción #<b>  {{offer.id}} </b> en el mensaje de su transferencia. 
        <br>
-       O puede enviar evidencia de transaccion <b> alejandro2141@gmail.com </b>
+       Tambien puede enviar información de la transaccion al correo: <b> alejandro2141@gmail.com </b>
        <br>
-       O puede enviar evidencia whatsApp a <b>+56975397200</b>
+       o al whatsApp a <b>+56975397200</b>
        <br>
 
 
        
     </div>
     
-    
+    <!--
     <br>
 
-    <div class="border border-2 p-4 w-50 text-center"> WebPay </div>
-    
+    <div class="border text-secondary border-2 p-4 w-50 text-center border-secondary"> WebPay </div>
+  -->
 
 </div>
 
