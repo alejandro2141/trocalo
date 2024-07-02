878960c293c3eec349dfa2d79f8a7ca965f5a40f
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-02 00:39:09 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -57,6 +57,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/admin.vue')
+    },
+    {
+      path: '/ErrorSession',
+      name: 'ErrorSession',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ErrorSession.vue')
     }
 
 
