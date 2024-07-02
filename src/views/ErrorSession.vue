0e9919a4d0fdd28443031f073d54b3c3671fb44d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-02 00:39:06 -0400

    Create ErrorSession.vue

---
+++ src/views/ErrorSession.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+ 
+ <div class="text-center">
+
+    <h4><i class="bi bi-sign-stop display-3"></i>
+        <br>
+        Sesión Inválida<br> 
+        Se hizo login desde otro dispositivo
+    </h4>
+
+   
+
+</div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+export default {
+
+  data : function() {
+      return {
+       novar : false, 
+
+      }
+  },
+  props: ['session_data'],
+  emits: ['goToLogin'],
+
+created() {
+
+    //this.session_data = null 
+    //this.$emit('goToLogin'); 
+
+    },
+
+methods: {
+   
+    },
+
+watch : {
+      }
+}
+</script>
+
+
