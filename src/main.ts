beb5d50704d2a4523e3be9bc5666c99baea68394
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-02 00:39:12 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,30 @@ import VueAxios from 'vue-axios'
 const app = createApp(App)
 
 app.use(createPinia())
+
+
 app.use(router)
+
+
+//************************************************* */
+//************AXIOS INTERCEPTOR FOR RESPONSE ****** */
+//************************************************* */
+axios.interceptors.response.use((response) => {
+    
+    console.log("AXIOS RESPONSE main TS")
+
+    return response
+  }, (error) => {
+    // Handle errors here
+    console.log("INTERCEPTOR ERROR 401")
+    console.error(error)
+    //redirect to ERROR SESSION
+    router.push('ErrorSession') 
+    //return Promise.reject(error)
+  })
+//*********************************************** */
+//*********************************************** */
+
+
 
 app.mount('#app')
