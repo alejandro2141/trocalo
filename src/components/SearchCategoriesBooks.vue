6c3ca7f10dac2cbcdc8dc7208a065a5b2e250aba
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-18 00:40:00 -0400

    Update SearchCategoriesBooks.vue

--- src/components/SearchCategoriesBooks.vue
+++ src/components/SearchCategoriesBooks.vue
@@ -56,7 +56,7 @@ export default {
       }
   },
   props: ['session_data'],
-  emits: ['showPublicObjectDetails'],
+  emits: ['showPublicObjectDetails','filterByCategory'],
 
 created() {
     console.log("Search Categories Games")
