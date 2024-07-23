6bf8c782dff5f908c54bdc2ba57db6fb36b02fff
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-23 00:48:59 -0400

    Update PaymentProcessStart.vue

--- src/components/PaymentProcessStart.vue
+++ src/components/PaymentProcessStart.vue
@@ -22,8 +22,61 @@ import axios from 'axios'
     <br>
     <br>
 
+
+
+
+<button @click="showPaymentOnceDeliver=!showPaymentOnceDeliver ;showTransferData = false  " type="button" class="btn" :class="{ 'btn-success': showPaymentOnceDeliver ,  'btn-secondary': !showPaymentOnceDeliver  }">
+  <i class="bi bi-cash-stack "></i>  
+  Efectivo contra entrega
+</button>
+
+    <div v-if="showPaymentOnceDeliver==true"> 
+      <br>
+      Registraremos que el pago se realizará contra entrega del objeto. 
+      <br>
+      
+   
+    </div>
+
+
+<br>
+<br>
+
+
+<button @click="showTransferData=!showTransferData; showPaymentOnceDeliver = false " type="button" class="btn" :class="{ 'btn-success': showTransferData ,  'btn-secondary': !showTransferData  }">
+  <i class="bi bi-bank"></i>  
+  Transferencia
+</button>
+
+    <div v-if="showTransferData==true"> 
+      <br>
+      Debe realizar una transaccion a la siguiente cuenta:  
+      <br>
+      <ul>
+        <li> Nombre:  Juan Morales</li>
+        <li> Rut:  13.909.371-2</li>
+        <li>Banco:  Banco de Chile</li>
+        <li>Tipo:  Cuenta Corriente</li>
+        <li>Numero Cta: 9626000</li>
+        <li>Indicando numero #<b>{{offer.id}}</b> en el mensaje de su transferencia. </li>
+     </ul>
+    </div>
+
+
+
+<div class="w-100 d-flex justify-content-center" style="position: fixed; bottom:0%;">
+    <button @click="" type="button" class="btn btn-success">
+        Continuar
+    </button>
+</div>
+
+
+
+
+
+<!--
     <div class="d-flex justify-content-center">
-      <div class="border border-2 p-4 w-50 text-center" @click="showTransferData=!showTransferData"> Transferencia </div>
+       <div class="border border-2 p-4 w-50 text-center" @click="showTransferData=!showTransferData">  Transferencia </div>
     </div>
 
     <div v-if="showTransferData==true"> 
@@ -43,11 +96,39 @@ import axios from 'axios'
        <br>
        o al whatsApp a <b>+56975397200</b>
        <br>
+    </div>
+    
+
+
+    <div class="d-flex justify-content-center">
+      <div class="border border-2 p-4 w-50 text-center" @click="showTransferData=!showTransferData">  Pago Contra Entrega </div>
+    </div>
+
+    <div v-if="showTransferData==true"> 
+      Deberá cancelar el monto {{offer.amount}} 
+      <br>
+
+       Nombre:  Juan Morales<br>
+       Rut:  13.909.371-2<br>
+       Banco:  Banco de Chile<br>
+       Tipo:  Cuenta Corriente<br>
+       Numero Cta: 9626000<br>
+       <br>
+       <text>Importante.</text> <br>
+       Debe indicar el numero de transacción #<b>  {{offer.id}} </b> en el mensaje de su transferencia. 
+       <br>
+       Tambien puede enviar información de la transaccion al correo: <b> alejandro2141@gmail.com </b>
+       <br>
+       o al whatsApp a <b>+56975397200</b>
+       <br>
 
 
        
     </div>
-    
+
+
+-->
+
     <!--
     <br>
 
@@ -70,6 +151,7 @@ export default {
   data : function() {
       return {
         showTransferData : false ,
+        showPaymentOnceDeliver : false ,
         }
   },
   props: ['session_data', 'offer' ],
