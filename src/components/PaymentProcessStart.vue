1bfb62afe00653c8bf2e4ffcefba9cf29aed6153
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-23 23:25:35 -0400

    Update PaymentProcessStart.vue

--- src/components/PaymentProcessStart.vue
+++ src/components/PaymentProcessStart.vue
@@ -10,66 +10,112 @@ import axios from 'axios'
 
 <div    style="width: 350px;" >
 
-    <div class="d-flex justify-content-between">
-      <i style="font-size:36px"  class="bi bi-cash-coin text-warning"></i> 
-      <i  @click="$emit('close')" class="display-1 bi bi-x-lg"></i>      
-    </div>
-
-    Actualmente contamos con las siguientes formas de pago. 
+  <div v-if="showSelectPaymentMeans">
 
 
-    <br>
-    <br>
-    <br>
-
-
-
-
-<button @click="showPaymentOnceDeliver=!showPaymentOnceDeliver ;showTransferData = false  " type="button" class="btn" :class="{ 'btn-success': showPaymentOnceDeliver ,  'btn-secondary': !showPaymentOnceDeliver  }">
-  <i class="bi bi-cash-stack "></i>  
-  Efectivo contra entrega
-</button>
-
-    <div v-if="showPaymentOnceDeliver==true"> 
+      <div class="d-flex justify-content-between">
+        <i style="font-size:36px"  class="bi bi-cash-coin text-warning"></i> 
+        <i  @click="$emit('close')" class="display-1 bi bi-x-lg"></i>      
+      </div>
+      Actualmente contamos con las siguientes formas de pago. 
       <br>
-      Registraremos que el pago se realizará contra entrega del objeto. 
       <br>
-      
-   
-    </div>
-
-
-<br>
-<br>
-
-
-<button @click="showTransferData=!showTransferData; showPaymentOnceDeliver = false " type="button" class="btn" :class="{ 'btn-success': showTransferData ,  'btn-secondary': !showTransferData  }">
-  <i class="bi bi-bank"></i>  
-  Transferencia
-</button>
-
-    <div v-if="showTransferData==true"> 
       <br>
-      Debe realizar una transaccion a la siguiente cuenta:  
+      <button @click="showPaymentOnceDeliver=!showPaymentOnceDeliver ;showTransferData = false ; buttonMsg = 'Contra Entrega' " type="button" class="btn" :class="{ 'btn-success': showPaymentOnceDeliver ,  'btn-secondary': !showPaymentOnceDeliver  }">
+        <i class="bi bi-cash-stack "></i>  
+        Efectivo contra entrega
+      </button>
+          <div v-if="showPaymentOnceDeliver==true"> 
+            <br>
+            Registraremos que el pago se realizará contra entrega del objeto. 
+            <br>
+          </div>
       <br>
-      <ul>
-        <li> Nombre:  Juan Morales</li>
-        <li> Rut:  13.909.371-2</li>
-        <li>Banco:  Banco de Chile</li>
-        <li>Tipo:  Cuenta Corriente</li>
-        <li>Numero Cta: 9626000</li>
-        <li>Indicando numero #<b>{{offer.id}}</b> en el mensaje de su transferencia. </li>
-     </ul>
-    </div>
+      <br>
+      <button @click="showTransferData=!showTransferData; showPaymentOnceDeliver = false ; buttonMsg = 'Transferencia'" type="button" class="btn" :class="{ 'btn-success': showTransferData ,  'btn-secondary': !showTransferData  }">
+        <i class="bi bi-bank"></i>  
+        Transferencia
+      </button>
+
+          <div v-if="showTransferData==true"> 
+            <br>
+            En un plazo maximo de <text class="text-warning">2 dias</text> <br>
+            Usted debera realizar una transaccion a la siguiente cuenta:  
+            <br>
+            <ul>
+              <li> Nombre:  Juan Morales</li>
+              <li> Rut:  13.909.371-2</li>
+              <li>Banco:  Banco de Chile</li>
+              <li>Tipo:  Cuenta Corriente</li>
+              <li>Numero Cta: 9626000</li>
+              <li>Indicando numero #<b>{{offer.id}}</b> en el mensaje de su transferencia. </li>
+           </ul>
+          </div>
 
 
+      <div class="w-100 d-flex text-center m-3" style="position: fixed; bottom:0%;">
+          <button @click="sendPaymentMethod(); showSelectPaymentMeans=false"  type="button"  class="btn btn-success">
+              Continuar {{buttonMsg}}
+          </button>
 
-<div class="w-100 d-flex justify-content-center" style="position: fixed; bottom:0%;">
-    <button @click="" type="button" class="btn btn-success">
-        Continuar
-    </button>
+      </div>
+
+
 </div>
 
+<!-- PAY TO DELIVER-->
+<div v-if="showPaymentOnceDeliver && showSelectPaymentMeans == false ">
+
+  <br>
+  <br>
+    <p class="text-center">
+      <i class="bi bi-emoji-kiss" style="font-size:46px"></i>
+    </p>
+  <br>
+  
+  Tu opcion de pago ha sido registrada.  <br>
+  Usted debera pagar <text class="text-warning">$ {{offer.amount}} </text> a nuestro Repartidor. 
+    <p>
+      Desde ahora puede seguir este Intercambio en Enviadas-> En Despacho 
+    </p>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <p class="text-center">
+    <button @click="console.log('redirecto to view proposal'); $router.push({ name: 'Welcome' })"  type="button" class="btn btn-success">Ir al Inicio</button>
+  </p>
+
+</div>
+
+<!-- PAY TRANSFER -->
+<div v-if="showTransferData  && showSelectPaymentMeans == false">
+
+  <br>
+  <br>
+    <p class="text-center">
+      <i class="bi bi-emoji-kiss" style="font-size:46px"></i>
+    </p>
+  <br>
+  
+  Tu opcion de pago ha sido registrada.  <br>
+  Usted tiene <text class="text-warning"> 2 dias </text> para  transferir <text class="text-warning">$ {{offer.amount}} </text> a la cuenta indicada. 
+  <br>
+  Hemos enviado a su correo registrado los datos de transferencia.  
+    <p>
+      Desde ahora puede seguir este Intercambio en Enviadas-> En Despacho 
+    </p>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <p class="text-center">
+    <button @click="console.log('redirecto to view proposal'); $router.push({ name: 'Welcome' })"  type="button" class="btn btn-success">Ir al Inicio</button>
+  </p>
+
+</div>
 
 
 
@@ -152,6 +198,11 @@ export default {
       return {
         showTransferData : false ,
         showPaymentOnceDeliver : false ,
+        buttonMsg : "" , 
+        showSelectPaymentMeans : true ,
+        PaymentBankTransfer : false ,
+        PaymentOnceDeliver : false ,
+
         }
   },
   props: ['session_data', 'offer' ],
@@ -161,6 +212,41 @@ created() {
           },
 
 methods: {
+
+      async sendPaymentMethod()
+      {
+        let payment_type = 0 
+
+        if (this.showTransferData)
+        {
+          payment_type = 1
+        }
+
+        if (this.showPaymentOnceDeliver)
+        {
+          payment_type = 2
+        }
+
+       console.log("Send Payment Method") 
+       let json_request = 
+        {
+          session_data : this.session_data,
+          proposal_id  : this.offer.id ,
+          payment_type : payment_type   
+        }
+
+        let jsonResponse = await axios.post(BKND_CONFIG.BKND_HOST+"/private_update_proposal_payment", json_request);
+        console.log("/private_update_proposal_payment Response:"+JSON.stringify(jsonResponse.data))
+    
+        let response = jsonResponse.data
+
+      },
+
+
+
+
+
+
         },
 
 watch : {
