ed7db591034bcc6d363c379fb9b92105d4c40f77
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-29 16:46:00 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,6 +21,15 @@ const router = createRouter({
       props: true,
       component: () => import('../views/Welcome.vue')
     },
+
+    {
+      path: '/RegisterFormWithInvitation',
+      name: 'RegisterFormWithInvitation',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/RegisterFormWithInvitation.vue')
+    },
    
     {
       path: '/ViewLogin',
