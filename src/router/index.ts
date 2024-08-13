e3aaf8f692962d0dcbc1e1037b6a2dfa06bab4d1
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-08-13 12:01:11 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -73,12 +73,12 @@ const router = createRouter({
       component: () => import('../views/ViewProposalsSent.vue')
     },
     {
-      path: '/admin',
-      name: 'admin',
+      path: '/Monitoring_Admin',
+      name: 'Monitoring_Admin',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/admin.vue')
+      component: () => import('../views/Monitoring_Admin.vue')
     },
     {
       path: '/ErrorSession',
