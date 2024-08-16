a71f8ff42466e476f0a417ec8af481c8d72b2010
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-08-16 13:32:06 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,7 +28,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       //props: true,
-      params: true,
       component: () => import('../views/Welcome.vue')
     },
 
