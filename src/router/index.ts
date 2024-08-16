ef12aa04e9a80005e8341be62f6f7d6d9db08f1d
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-08-15 20:27:43 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,6 @@ import Welcome from '../views/Welcome.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  
   routes: [
     {
       path: '/',
@@ -19,6 +18,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       props: true,
+      component: () => import('../views/Welcome.vue')
+    },
+
+    {
+      path: '/ViewObjectFromSocial',
+      name: 'ViewObjectFromSocial',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //props: true,
+      params: true,
       component: () => import('../views/Welcome.vue')
     },
 
