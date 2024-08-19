d96c877f128dfa4dcac8790867d8bd874ecdb0de
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-08-19 01:15:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,9 @@ let BKND_REGIONS = [] ;
 let BKND_CONFIG = {
     //BKND_HOST : "HTTP://ec2-3-143-168-51.us-east-2.compute.amazonaws.com:8080",
     //local ENv
-    BKND_HOST : "https://reusar.cl:8084",
-   // BKND_HOST : "http://localhost:8080",
-    
-    //DEVEL
-    //BKND_HOST : "https://www.horapo.com:8443",
-    //PROD
-    //BKND_HOST : "https://www.horapo.com:8443",
+    //BKND_HOST : "https://reusar.cl:8084",
+    BKND_HOST : "http://localhost:8080",
+   
     BKND_TIME : 1000,
     BNKD_USER : 'abc'
   };
